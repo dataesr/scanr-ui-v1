149b71e351800f11ccd3ad180f4eea3afe74271b
Jerem <jeremy.peglion@gmail.com>
2018-10-02 11:53:49 +0200

    Ajout du composant statusTag + correction webpack codage des classes + ellipis adresse dans les cards de la liste de structure

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -166,7 +166,7 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                   modules: true,
-                  localIndentName: '[name]__[local]__[hash:base64:5]'
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
               },
               {
