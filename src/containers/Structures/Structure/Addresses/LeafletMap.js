5597cc9ad60d7dcc4612ad04d5677aa04a2925d0
ambreelmee <ambreelmee@gmail.com>
2018-10-04 18:33:24 +0200

    Display all markers

--- src/containers/Structures/Structure/Addresses/LeafletMap.js
+++ src/containers/Structures/Structure/Addresses/LeafletMap.js
@@ -1,58 +1,89 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import * as L from 'leaflet';
 import PropTypes from 'prop-types';
-import { redIcon } from './Icons';
-
+import { greenIcon, blueIcon, greyIcon } from './Icons';
 import Aux from '../../../../hoc/Aux';
 import Button from '../../../../UI/Button/Button';
 
+const getIconColor = (status) => {
+  switch (status) {
+    case 'main':
+      return greenIcon;
+    case 'valid':
+      return blueIcon;
+    case 'old':
+      return greyIcon;
+    default:
+      return blueIcon;
+  }
+};
+
 class LeafletMap extends Component {
   state = {
-    zoom: 16,
     latlng: null,
+    bounds: null,
   };
-
+  // feature temporarly removed
   onClick = (event) => {
     this.setState({
       latlng: event.latlng,
     });
   }
 
-  render() {
+  componentDidMount() {
+    const markerArray = []
+    this.props.displayedAddresses.map(address => markerArray.push(address.coordinates))
+    const bounds = L.latLngBounds(markerArray);
+    this.setState({ bounds })
+  }
 
-    const marker = this.state.latlng ? (
-      <Marker position={this.state.latlng}>
+  renderMarker() {
+    return this.props.displayedAddresses.map(address => (
+      <Marker
+        key={address.id}
+        position={address.coordinates}
+        icon={getIconColor(address.status)}
+      >
         <Popup>
-          <span>Ajuster la géolocalisation ?</span>
+          <div>
+            {`${address.geocoder_address.house_number} ${address.geocoder_address.street}`}
+            <br />
+            {`${address.geocoder_address.post_code}, ${address.geocoder_address.city}`}
+            <br />
+            {address.geocoder_address.country}
+          </div>
         </Popup>
-      </Marker>
-    ) : null;
-    const position = [this.props.latitude, this.props.longitude];
+      </Marker>));
+  }
+
+  render() {
+    let marker = null;
     let saveButton = null;
-    if (this.state.latlng) {
-      saveButton = <Button>Corriger les coordonnées gps</Button>;
-    }
+    // const marker = this.state.latlng ? (
+    //   <Marker position={this.state.latlng}>
+    //     <Popup>
+    //       <span>Ajuster la géolocalisation ?</span>
+    //     </Popup>
+    //   </Marker>
+    // ) : null;
+    // let saveButton = null;
+    // if (this.state.latlng) {
+    //   saveButton = <Button>Corriger les coordonnées gps</Button>;
+    // }
+
     return (
       <Aux>
         <Map
-          center={position}
-          zoom={this.state.zoom}
           onClick={this.onClick}
+          bounds={this.state.bounds}
+          boundsOptions={{ padding: [50, 50] }}
         >
           <TileLayer
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker
-            position={position}
-            icon={redIcon}
-          >
-            <Popup>
-              <div>
-                {this.props.formattedAddress}
-              </div>
-            </Popup>
-          </Marker>
+          {this.renderMarker()}
           {marker}
         </Map>
         {saveButton}
@@ -62,9 +93,7 @@ class LeafletMap extends Component {
 }
 
 LeafletMap.propTypes = {
-  formattedAddress: PropTypes.object.isRequired,
-  latitude: PropTypes.number.isRequired,
-  longitude: PropTypes.number.isRequired,
-}
+  displayedAddresses: PropTypes.array.isRequired,
+};
 
 export default LeafletMap;
