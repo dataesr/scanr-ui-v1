26c2fa2e03064e5918fb3ea5f54a8213a0194cc5
ambreelmee <ambreelmee@gmail.com>
2018-10-10 11:18:11 +0200

    Improve UX and make edition work

--- src/containers/Structures/Structure/Addresses/Address/AddressDispatcher.js
+++ src/containers/Structures/Structure/Addresses/Address/AddressDispatcher.js
@@ -3,31 +3,59 @@ import PropTypes from 'prop-types';
 
 import AddressMini from './AddressMini';
 import AddressFull from './AddressFull';
-import AddressFullEdit from './AddressFullEdit';
 
 class AddressDispatcher extends Component {
   state = {
-    displayMode: 'mini ',
+    displayMode: 'mini',
   };
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.editedAddress && this.props.editedAddress
+        && prevProps.editedAddress !== this.props.editedAddress
+        && this.props.editedAddress.id !== this.props.address.id) {
+      this.setState({ displayMode: 'mini' });
+    }
+  }
 
   changeDisplayModeHandler = (mode) => {
     this.setState({ displayMode: mode });
+    if (mode === 'full') {
+      this.props.setEditedAddress(this.props.address);
+    } else {
+      this.props.setEditedAddress(null);
+    }
   }
 
   render() {
     let content = '';
     switch (this.state.displayMode) {
       case 'mini':
-        content = <AddressMini address={this.props.address} changeDisplayMode={this.changeDisplayModeHandler} />;
+        content = (
+          <AddressMini
+            address={this.props.address}
+            changeDisplayMode={() => this.changeDisplayModeHandler('full')}
+            mouseOut={this.props.mouseOut}
+            mouseOver={this.props.mouseOver}
+          />);
         break;
       case 'full':
-        content = <AddressFull address={this.props.address} changeDisplayMode={this.changeDisplayModeHandler} />;
-        break;
-      case 'fullEdit':
-        content = <AddressFullEdit address={this.props.address} changeDisplayMode={this.changeDisplayModeHandler} />;
+        content = (
+          <AddressFull
+            address={this.props.address}
+            changeDisplayMode={() => this.changeDisplayModeHandler('mini')}
+            editAddress={this.props.editAddress}
+            deleteButton={this.props.deleteButton}
+            hasErrored={this.props.hasErrored}
+          />);
         break;
       default:
-        content = <AddressMini address={this.props.address} changeDisplayMode={this.changeDisplayModeHandler} />;
+        content = (
+          <AddressMini
+            address={this.props.address}
+            changeDisplayMode={() => this.changeDisplayModeHandler('full')}
+            mouseOut={this.props.mouseOut}
+            mouseOver={this.props.mouseOver}
+          />);
     }
 
     return content;
@@ -38,4 +66,11 @@ export default AddressDispatcher;
 
 AddressDispatcher.propTypes = {
   address: PropTypes.object.isRequired,
+  deleteButton: PropTypes.func.isRequired,
+  editAddress: PropTypes.func.isRequired,
+  editedAddress: PropTypes.object,
+  hasErrored: PropTypes.bool.isRequired,
+  mouseOut: PropTypes.func.isRequired,
+  mouseOver: PropTypes.func.isRequired,
+  setEditedAddress: PropTypes.func.isRequired,
 };
