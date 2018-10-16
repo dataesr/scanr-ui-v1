9106a5d1121180ec780250dca55e023662d4339c
ambreelmee <ambreelmee@gmail.com>
2018-10-16 14:39:16 +0200

    Prefill AddressFull with new address

--- src/containers/Structures/Structure/Addresses/Address/AddressFull.js
+++ src/containers/Structures/Structure/Addresses/Address/AddressFull.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 /* Composants internes */
+import Aux from '../../../../../hoc/Aux';
 import Button from '../../../../../UI/Button/Button';
 import ErrorMessage from '../../../../../UI/Messages/ErrorMessage';
 import FieldTitle from '../../../../../UI/Field/FieldTitle';
@@ -15,14 +16,15 @@ import classes from './Address.scss';
 class AddressFull extends Component {
   state={
     address: {
-      address_1: this.props.address.address_1 || '',
-      postal_code: this.props.address.postal_code || '',
-      city_code: this.props.address.city_code || '',
+      housenumber: this.props.address.housenumber || this.props.address.house_number || '',
+      street: this.props.address.street || '',
+      postcode: this.props.address.postcode || this.props.address.post_code || '',
+      citycode: this.props.address.citycode || this.props.address.city_code || '',
       city: this.props.address.city || '',
       country: this.props.address.country || '',
-      status: this.props.address.status,
+      status: this.props.status,
     },
-    editMode: false,
+    editMode: this.props.editMode,
   }
 
   componentDidUpdate(prevProps) {
@@ -56,7 +58,7 @@ class AddressFull extends Component {
 
   saveButton = () => {
     const newAddress = { ...this.props.address, ...this.state.address };
-    this.props.editAddress(newAddress);
+    this.props.saveAddress(newAddress);
   }
 
   render() {
@@ -70,8 +72,8 @@ class AddressFull extends Component {
     }
     return (
       <div className={classes.Address}>
-        <ErrorMessage visible={this.props.hasErrored}>"Erreur lors de l'envoi du formulaire"</ErrorMessage>
-        <div className="columns is-gapless is-multiline is-marginless">
+        <ErrorMessage visible={this.props.hasErrored}>Erreur lors de l&#39;envoi du formulaire</ErrorMessage>
+        <div className={`columns is-multiline is-marginless ${this.state.editMode ? null : 'is-gapless'}`}>
           <FieldTitle columnSize="10">
             Informations générales
           </FieldTitle>
@@ -79,7 +81,6 @@ class AddressFull extends Component {
             <Button onClick={this.cancelButton}>
               <i className="fas fa-undo" />
             </Button>
-            {saveButton}
           </div>
           <StatusField
             editMode={this.state.editMode}
@@ -89,17 +90,25 @@ class AddressFull extends Component {
           />
           <StringField
             editMode={this.state.editMode}
-            fieldValue={this.state.address.address_1}
-            id="address_1"
-            label="Addresse"
+            fieldValue={this.state.address.housenumber}
+            id="housenumber"
+            label="Numéro"
+            onChange={this.onChange}
+            onClick={() => this.setDisplayMode(true)}
+          />
+          <StringField
+            editMode={this.state.editMode}
+            fieldValue={this.state.address.street}
+            id="rue"
+            label="Rue"
             onChange={this.onChange}
             onClick={() => this.setDisplayMode(true)}
           />
           <StringField
             columnSize="3"
             editMode={this.state.editMode}
-            fieldValue={this.state.address.postal_code}
-            id="postal_code"
+            fieldValue={this.state.address.postcode}
+            id="postcode"
             label="Code postal"
             onChange={this.onChange}
             onClick={() => this.setDisplayMode(true)}
@@ -107,8 +116,8 @@ class AddressFull extends Component {
           <StringField
             columnSize="3"
             editMode={this.state.editMode}
-            fieldValue={this.state.address.city_code}
-            id="city_code"
+            fieldValue={this.state.address.citycode}
+            id="citycode"
             label="Code commune"
             onChange={this.onChange}
             onClick={() => this.setDisplayMode(true)}
@@ -135,7 +144,7 @@ class AddressFull extends Component {
           <StringField
             columnSize="3"
             editMode={false}
-            fieldValue={this.props.address.coordinates ? this.props.address.coordinates[1].toFixed(5) : '.'}
+            fieldValue={this.props.coordinates ? this.props.coordinates[1].toFixed(5) : '.'}
             label="Longitude"
             onChange={this.onChange}
             onClick={() => this.setDisplayMode(true)}
@@ -143,25 +152,32 @@ class AddressFull extends Component {
           <StringField
             columnSize="3"
             editMode={false}
-            fieldValue={this.props.address.coordinates ? this.props.address.coordinates[0].toFixed(5) : '.'}
+            fieldValue={this.props.coordinates ? this.props.coordinates[0].toFixed(5) : '.'}
             label="Latitude"
             onChange={this.onChange}
             onClick={() => this.setDisplayMode(true)}
           />
-          <FieldTitle>
-            Cycle de vie
-          </FieldTitle>
-          <LifeCycle
-            created_at={this.props.address.created_at}
-            created_by={this.props.address.created_by}
-            modified_at={this.props.address.modified_at}
-            modified_by={this.props.address.modified_by}
-          />
-        </div>
-        <div className="column is-1">
-          <Button onClick={this.props.deleteButton}>
-            <i className="fas fa-trash-alt" />
-          </Button>
+          {this.props.lifecycle ? (
+            <Aux>
+              <FieldTitle>
+                Cycle de vie
+              </FieldTitle>
+              <LifeCycle
+                created_at={this.props.lifecycle.created_at}
+                created_by={this.props.lifecycle.created_by}
+                modified_at={this.props.lifecycle.modified_at}
+                modified_by={this.props.lifecycle.modified_by}
+              />
+            </Aux>) : null}
+          <div className="column is-8" />
+          <div className="column is-2 has-text-right">
+            <Button onClick={this.props.deleteButton}>
+              <i className="fas fa-trash-alt" />
+            </Button>
+          </div>
+          <div className="column is-2 has-text-right">
+            {saveButton}
+          </div>
         </div>
       </div>
     );
@@ -174,7 +190,11 @@ export default AddressFull;
 AddressFull.propTypes = {
   address: PropTypes.object.isRequired,
   changeDisplayMode: PropTypes.func,
+  coordinates: PropTypes.array,
   deleteButton: PropTypes.func.isRequired,
-  editAddress: PropTypes.func.isRequired,
+  editMode: PropTypes.bool.isRequired,
   hasErrored: PropTypes.bool.isRequired,
+  lifecycle: PropTypes.object,
+  saveAddress: PropTypes.func.isRequired,
+  status: PropTypes.string,
 };
