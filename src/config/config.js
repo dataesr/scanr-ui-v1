92a1bc1855a6c560fee3c930b6b57ab4b2e9ecc5
jeremy.peglion <jeremy.peglion@gmail.com>
2018-10-18 15:29:57 +0200

    Fix button display

--- src/config/config.js
+++ src/config/config.js
@@ -5,15 +5,16 @@ export const PAGE = 1;
 export const PER_PAGE = 12;
 
 /* Statuts */
-export const STATUS_ARRAY = ['main', 'active', 'old'];
 export const STATUS_MAIN = 'main';
 export const STATUS_ACTIVE = 'active';
-export const STATUS_VALID = 'valid';
-export const STATUS_INVALID = 'invalid';
 export const STATUS_OLD = 'old';
+export const STATUS_ARRAY = [STATUS_MAIN, STATUS_ACTIVE, STATUS_OLD];
 
+/* ERREURS */
 export const ERREUR_STATUT = 'Erreur statut';
 export const ERREUR_NULL = 'Champ vide';
+
 /* API */
-export const API_END_POINT = 'http://10.243.98.107:5002/api/v0.1/datastore/';
-//export const API_END_POINT = 'http://0.0.0.0:5000/api/v0.1/datastore/';
+// export const API_END_POINT = 'http://0.0.0.0:5000/api/v0.1/datastore/';
+// export const API_END_POINT = 'http://10.243.98.107:5002/api/v0.1/datastore/';
+export const API_END_POINT = 'http://10.243.98.15:5000/api/v1/';
