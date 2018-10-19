6b3e295f2f56c8e813c252d67a2b8984e73eff64
ambreelmee <ambreelmee@gmail.com>
2018-10-19 17:26:11 +0200

    Add enag in axios calls

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import { API_END_POINT } from './config/config';
 
-const instance = axios.create({
-  baseURL: API_END_POINT,
-  headers: { 'If-Match': localStorage.getItem('etag') },
+const instance = axios.create({ baseURL: API_END_POINT });
+instance.interceptors.request.use((request) => {
+  request.headers['If-Match'] = localStorage.getItem('etag');
+  return request;
 });
-// const instance = axios.create({ baseURL: API_END_POINT, headers: {'X-Fields': 'names, addresses'} });
+instance.interceptors.response.use((response) => {
+  localStorage.setItem('etag', response.data.etag);
+  return response;
+});
 
 export default instance;
