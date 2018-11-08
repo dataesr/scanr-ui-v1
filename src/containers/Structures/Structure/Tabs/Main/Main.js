cf7a2d308272abecba73d2c767b0bdc5be8ab511
ambreelmee <ambreelmee@gmail.com>
2018-11-08 10:41:40 +0100

    Change tab structure

--- src/containers/Structures/Structure/Tabs/Main/Main.js
+++ src/containers/Structures/Structure/Tabs/Main/Main.js
@@ -3,12 +3,7 @@ import PropTypes from 'prop-types';
 
 // Descriptions
 import Descriptions from '../../../../../config/descriptions/structure/descriptions';
-import EmailsDescription from '../../../../../config/descriptions/structure/emails';
-import ExternalLinksDescription from '../../../../../config/descriptions/structure/externalLinks';
 import NameDescription from '../../../../../config/descriptions/structure/names';
-import PhonesDescription from '../../../../../config/descriptions/structure/phones';
-import SocialMediasDescription from '../../../../../config/descriptions/structure/socialMedias';
-import WebsitesDescription from '../../../../../config/descriptions/structure/websites';
 // Composants UI
 import GridFields from '../../../../Fields/GridFields/GridFields';
 import TagField from '../../../../Fields/TagField/TagField';
@@ -49,45 +44,6 @@ const Main = props => (
         title="Codes"
       />
     </div>
-
-    <div className="column  is-6">
-      <GridFields
-        data={props.emails}
-        description={EmailsDescription}
-        refreshFunction={props.getStructure}
-        infoMessage="Aucun email actif"
-        label="email"
-        schemaName="emails"
-        url={`structures/${props.structureId}`}
-        title="Emails"
-      />
-    </div>
-
-    <div className="column is-6">
-      <GridFields
-        data={props.phones}
-        description={PhonesDescription}
-        refreshFunction={props.getStructure}
-        infoMessage="Aucun téléphone actif"
-        label="téléphone"
-        schemaName="phones"
-        url={`structures/${props.structureId}`}
-        title="Téléphones"
-      />
-    </div>
-
-    <div className="column is-6">
-      <GridFields
-        data={props.social_medias}
-        description={SocialMediasDescription}
-        refreshFunction={props.getStructure}
-        infoMessage="Aucun réseau social"
-        label="réseau social"
-        schemaName="social_medias"
-        url={`structures/${props.structureId}`}
-        title="Réseaux sociaux"
-      />
-    </div>
     <div className="column is-12">
       <GridFields
         data={props.descriptions}
@@ -100,31 +56,6 @@ const Main = props => (
         title="Descriptions"
       />
     </div>
-    <div className="column is-6">
-      <GridFields
-        data={props.websites}
-        description={WebsitesDescription}
-        refreshFunction={props.getStructure}
-        infoMessage="Aucun site internet"
-        label="site internet"
-        schemaName="websites"
-        url={`structures/${props.structureId}`}
-        title="Sites internet"
-      />
-    </div>
-    <div className="column is-6">
-      <GridFields
-        data={props.external_links}
-        description={ExternalLinksDescription}
-        refreshFunction={props.getStructure}
-        infoMessage="Aucun lien externe"
-        label="lien externe"
-        schemaName="external_links"
-        url={`structures/${props.structureId}`}
-        title="Liens externes"
-      />
-    </div>
-
   </div>
 );
 
@@ -132,14 +63,9 @@ export default Main;
 
 Main.propTypes = {
   alias: PropTypes.array,
-  codeNumbers: PropTypes.array.isRequired,
+  codeNumbers: PropTypes.array,
   descriptions: PropTypes.array,
-  external_links: PropTypes.array,
-  emails: PropTypes.array,
-  getStructure: PropTypes.func.isRequired,
-  names: PropTypes.array.isRequired,
-  phones: PropTypes.array.isRequired,
-  social_medias: PropTypes.array,
-  structureId: PropTypes.string.isRequired,
-  websites: PropTypes.array,
+  getStructure: PropTypes.func,
+  names: PropTypes.array,
+  structureId: PropTypes.string,
 };
