1f338e14fc718c4c96d228e9e0c3dbc1e5589760
ambreelmee <ambreelmee@gmail.com>
2018-11-20 14:56:39 +0100

    change logo import and add sass-loader in prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -187,6 +187,9 @@ module.exports = {
                       },
                     },
                     {
+                      loader: require.resolve('sass-loader'),
+                    },
+                    {
                       loader: require.resolve('postcss-loader'),
                       options: {
                         // Necessary for external CSS imports to work
