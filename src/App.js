85fe7a4cd548090385bfb4a8f52839c625d825b5
Jerem <jeremy.peglion@gmail.com>
2018-12-12 22:29:36 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,115 +1,18 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Search from './containers/Search/Search';
+/* Composants */
+import HomePage from './Components/Home-page/Home-page';
+import SearchPage from './Components/Search-page/Search-page';
 
-// Structures
-import Structure from './containers/Structures/Structure/Structure';
-import StructureFilters from './config/descriptions/structure/filters';
-import StructuresGridItems from './containers/Structures/StructuresGridItems/StructuresGridItems';
 
-// Personnes
-import Person from './containers/Persons/Person/Person';
-import PersonFilters from './config/descriptions/person/filters';
-import PersonsGridItems from './containers/Persons/PersonsGridItems/PersonsGridItems';
-
-// Nomenclatures descriptions
-import Nomenclatures from './containers/Nomenclatures/Nomenclatures';
-import BadgesDescription from './config/descriptions/nomenclatures/badges';
-import PanelsDescription from './config/descriptions/nomenclatures/panelsERC';
-
-/* eslint-disable */
 const App = () => (
-  <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
-    <Switch>
-    {console.log('PUBLIC_URL', process.env.REACT_APP_PUBLIC_URL)}
-      <Route
-        path="/badges"
-        render={props => (
-          <Nomenclatures
-            {...props}
-            description={BadgesDescription}
-            defaulSortfield="name_fr"
-            infoMessage="vide"
-            labelAdd="Ajouter un badge"
-            schemaName="badges"
-            title="Badges"
-            urlAPI="badges"
-          />)}
-      />
-
-      <Route
-        path="/panelsERC"
-        render={props => (
-          <Nomenclatures
-            {...props}
-            description={PanelsDescription}
-            defaulSortfield="level"
-            infoMessage="vide"
-            labelAdd="Ajouter un panel ERC"
-            schemaName="panels"
-            title="Panel ERC"
-            urlAPI="panels"
-          />)}
-      />
-
-      <Route
-        path={['/structures/:id', '/structures']}
-        render={props => (
-          <Search
-            {...props}
-            entity="structures"
-            entityComponent={Structure}
-            filtersConfig={StructureFilters}
-            gridComponent={<StructuresGridItems />}
-            label="structure"
-          />)}
-      />
-      <Route
-        path={['/entreprises/:id', '/entreprises']}
-        render={props => (
-          <Search
-            {...props}
-            entity="entreprises"
-            filtersConfig={StructureFilters}
-            entityComponent={Structure}
-            gridComponent={<StructuresGridItems />}
-            label="entreprise"
-          />)}
-      />
-      <Route
-        path={['/institutions/:id', '/institutions']}
-        render={props => (
-          <Search
-            {...props}
-            entity="institutions"
-            filtersConfig={StructureFilters}
-            entityComponent={Structure}
-            gridComponent={<StructuresGridItems />}
-            label="institution"
-          />)}
-      />
-      <Route
-        path={['/persons/:id', '/persons']}
-        render={props => (
-          <Search
-            {...props}
-            entity="persons"
-            filtersConfig={PersonFilters}
-            entityComponent={Person}
-            gridComponent={<PersonsGridItems />}
-            label="personnes"
-          />)}
-      />
-      <Redirect from="/" to="structures" />
-    </Switch>
-  </BrowserRouter>
+  <Router>
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/search" component={SearchPage} />
+    </div>
+  </Router>
 );
-/* eslint-enable */
 
 export default App;
--- src/Utils/SortStatus.js
+++
@@ -1,5 +0,0 @@
-import { STATUS_ARRAY } from '../config/config';
-
-const sortStatus = (a, b) => STATUS_ARRAY.indexOf(a.status) - STATUS_ARRAY.indexOf(b.status);
-
-export default sortStatus;
