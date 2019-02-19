1c9bc6256c7e4f03862aa82da6b9bd8a4c2e492e
Jerem <jeremy.peglion@gmail.com>
2019-02-19 22:37:23 +0100

    separate file translation

--- src/Components/Shared/Header/Header-homePage.js
+++ src/Components/Shared/Header/Header-homePage.js
@@ -1,113 +1,127 @@
 import React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
 import PropTypes from 'prop-types';
+
+/* Gestion des langues */
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
+
+/* SCSS */
 import classes from './Header.scss';
+
 
 const Header = (props) => {
   const frActive = (props.language === 'fr') ? classes.ActiveLink : '';
   const enActive = (props.language === 'en') ? classes.ActiveLink : '';
 
-  return (
-    <section className={classes.Header}>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          ScanR_Icon
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+  const messages = {
+    fr: messagesFr,
+    en: messagesEn,
+  };
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <FormattedHTMLMessage
-                  id="Header.link.about"
-                  defaultMessage="Header.link.about"
-                />
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <div className="dropdown-divider" />
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                <FormattedHTMLMessage
-                  id="Header.link.search"
-                  defaultMessage="Header.link.search"
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <FormattedHTMLMessage
-                  id="Header.link.focus"
-                  defaultMessage="Header.link.focus"
-                />
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <FormattedHTMLMessage
-                  id="Header.link.cookies"
-                  defaultMessage="Header.link.cookies"
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <FormattedHTMLMessage
-                  id="Header.link.share"
-                  defaultMessage="Header.link.share"
-                />
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className={`${classes.Link} ${frActive}`}
-                onClick={() => props.switchLanguage('fr')}
-                onKeypress={() => props.switchLanguage('fr')}
-                role="button"
-                tabIndex={0}
-              >
-                FR
-              </a>
-              <span className={classes.PipeLink}>|</span>
-              <a
-                className={`${classes.Link} ${enActive}`}
-                onClick={() => props.switchLanguage('en')}
-                onKeypress={() => props.switchLanguage('fr')}
-                role="button"
-                tabIndex={0}
-              >
-                EN
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </section>
+  return (
+    <IntlProvider locale={props.language} messages={messages[props.language]}>
+      <section className={classes.Header}>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            ScanR_Icon
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <FormattedHTMLMessage
+                    id="Header.link.about"
+                    defaultMessage="Header.link.about"
+                  />
+                </a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="#">Action</a>
+                  <a className="dropdown-item" href="#">Another action</a>
+                  <div className="dropdown-divider" />
+                  <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  <FormattedHTMLMessage
+                    id="Header.link.search"
+                    defaultMessage="Header.link.search"
+                  />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <FormattedHTMLMessage
+                    id="Header.link.focus"
+                    defaultMessage="Header.link.focus"
+                  />
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <FormattedHTMLMessage
+                    id="Header.link.cookies"
+                    defaultMessage="Header.link.cookies"
+                  />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <FormattedHTMLMessage
+                    id="Header.link.share"
+                    defaultMessage="Header.link.share"
+                  />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className={`${classes.Link} ${frActive}`}
+                  onClick={() => props.switchLanguage('fr')}
+                  onKeyress={() => props.switchLanguage('fr')}
+                  role="button"
+                  tabIndex={0}
+                >
+                  FR
+                </a>
+                <span className={classes.PipeLink}>|</span>
+                <a
+                  className={`${classes.Link} ${enActive}`}
+                  onClick={() => props.switchLanguage('en')}
+                  onKeyPress={() => props.switchLanguage('fr')}
+                  role="button"
+                  tabIndex={0}
+                >
+                  EN
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </section>
+    </IntlProvider>
   );
 };
 
--- src/Components/Shared/ScanrToday/ScanrToday.js
+++ src/Components/Shared/ScanrToday/ScanrToday.js
@@ -1,46 +1,63 @@
 import React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
+import { IntlProvider, addLocaleData, FormattedHTMLMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+
+/* Gestion des langues */
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
 
 import CounterCardByType from '../CounterCards/CounterCardByType';
 
 import classes from './ScanrToday.scss';
 
-const ScanrToday = () => (
-  <div className={classes.ScanrToday}>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <span className={classes.Title}>
-            <FormattedHTMLMessage
-              id="ScanrToday.string.title"
-              defaultMessage="ScanrToday.string.title"
-            />
-            <i className="fa fa-info" />
-          </span>
+const ScanrToday = (props) => {
+  const messages = {
+    fr: messagesFr,
+    en: messagesEn,
+  };
+  return (
+    <IntlProvider locale={props.language} messages={messages[props.language]}>
+      <section className={classes.ScanrToday}>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <span className={classes.Title}>
+                <FormattedHTMLMessage
+                  id="ScanrToday.string.title"
+                  defaultMessage="ScanrToday.string.title"
+                />
+                <i className="fa fa-info" />
+              </span>
+            </div>
+            <div className="col">
+              <CounterCardByType
+                schema="entity"
+              />
+            </div>
+            <div className="col">
+              <CounterCardByType
+                schema="persons"
+              />
+            </div>
+            <div className="col">
+              <CounterCardByType
+                schema="projects"
+              />
+            </div>
+            <div className="col">
+              <CounterCardByType
+                schema="publications"
+              />
+            </div>
+          </div>
         </div>
-        <div className="col">
-          <CounterCardByType
-            schema="entity"
-          />
-        </div>
-        <div className="col">
-          <CounterCardByType
-            schema="persons"
-          />
-        </div>
-        <div className="col">
-          <CounterCardByType
-            schema="projects"
-          />
-        </div>
-        <div className="col">
-          <CounterCardByType
-            schema="publications"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
-);
+      </section>
+    </IntlProvider>
+  );
+};
 
 export default ScanrToday;
+
+ScanrToday.propTypes = {
+  language: PropTypes.string.isRequired,
+};
