3d2ab323710e81067099414a92efb0f5f00f03af
Jerem <jeremy.peglion@gmail.com>
2019-02-19 22:37:23 +0100

    separate file translation

--- src/Components/Home-page/Search/Search.js
+++ src/Components/Home-page/Search/Search.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import classes from './Search.scss';
+import Background from './poudre-header-home-yellow.jpg';
+
+const sectionStyle = {
+  backgroundImage: `url(${Background})`,
+};
 
 const Search = () => (
-  <div className={classes.Search}>
+  <div style={sectionStyle} className={classes.Search}>
+
     Search HomePage Component
 
   </div>
