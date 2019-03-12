20f33a7f17f273e5bce24aeb2587af5e9d3ae35b
miarkt <rakoto.mk@gmail.com>
2019-03-12 18:28:19 +0100

    création dossier Other-pages et LogoCard

---
+++ src/Components/Shared/Ui/LogoCard/LogoCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+/* SCSS */
+import classes from './LogoCard.scss';
+
+const LogoCard = () => (
+  <div className="card">
+    rien
+  </div>
+);
+
+export default LogoCard;
