c977be368e5a2efa8902153c251c19661e26b14f
Jerem <jeremy.peglion@gmail.com>
2019-03-27 18:49:26 +0100

    update homePage search component

---
+++ src/Components/Shared/Ui/Buttons/ButtonMiniDarkToSearch.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './ButtonMiniDarkToSearch.scss';
+
+const ButtonMiniDarkToSearch = props => (
+  <a
+    className={classes.ButtonMiniDarkToSearch}
+    url={props.url}
+  >
+    {props.children}
+  </a>
+);
+
+export default ButtonMiniDarkToSearch;
+
+ButtonMiniDarkToSearch.propTypes = {
+  children: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
