cdbf084b861b9e9df768761f59a756feb0635d23
jeremy.peglion <jeremy.peglion@gmail.com>
2019-04-02 11:01:18 +0200

    Ajout de nouvelles pages froides

--- src/Components/Other-pages/Contact/Contact.js
+++ src/Components/Other-pages/Contact/Contact.js
@@ -5,24 +5,22 @@ import PropTypes from 'prop-types';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header-homePage';
 import HeaderTitle from '../../Shared/HeaderTitle/HeaderTitle';
-import Banner from '../../Shared/Banner/Banner';
-import DiscoverThreeCards from '../../Shared/DiscoverThreeCards/DiscoverThreeCards';
 
 /* Gestion des langues */
 import messagesFr from './translations/fr.json';
 import messagesEn from './translations/en.json';
 
 /* SCSS */
-import classes from './Medias.scss';
+import classes from './Contact.scss';
 
 const messages = {
   fr: messagesFr,
   en: messagesEn,
 };
 
-const Medias = props => (
+const Contact = props => (
   <IntlProvider locale={props.language} messages={messages[props.language]}>
-    <div className={`container-fluid ${classes.Medias}`}>
+    <div className={`container-fluid ${classes.Contact}`}>
       <Header
         language={props.language}
         switchLanguage={props.switchLanguage}
@@ -30,28 +28,18 @@ const Medias = props => (
       <section>
         <HeaderTitle
           language={props.language}
-          label="media"
+          label="contact"
         />
-        <DiscoverThreeCards
-          language={props.language}
-          TitleCard1="Discover.TalkAboutScanr"
-          TitleCard2="Discover.Sources"
-          TitleCard3="Discover.Team"
-        />
-        <Banner
-          bannerColor={classes.BannerColor}
-          label="TalkAboutScanr"
-          language={props.language}
-        />
+
       </section>
       <Footer language={props.language} />
     </div>
   </IntlProvider>
 );
 
-export default Medias;
+export default Contact;
 
-Medias.propTypes = {
+Contact.propTypes = {
   language: PropTypes.string.isRequired,
   switchLanguage: PropTypes.func.isRequired,
 };
