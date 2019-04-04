a2d3eacb6012a305a1c3156ce5499ced8cf790b6
paulin <isabelle.paulin@recherche.gouv.fr>
2019-04-04 11:29:16 +0200

    update

---
+++ src/Components/Other-pages/Team-and-project/DiscoverScanR/DiscoverScanRCopie.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+
+import ComponentVideo from '../Video/ComponentVideo';
+
+/* Gestion des langues */
+import messagesFr from '../translations/fr.json';
+import messagesEn from '../translations/en.json';
+
+import Background from './poudre-blanche_Fbleu-A.jpg';
+import Poster from './paques.jpg';
+
+/* SCSS */
+import classes from './DiscoverScanR.scss';
+
+const sectionStyle = {
+  backgroundImage: `url(${Background})`,
+  backgroundPosition: '450px 150px',
+};
+
+const messages = {
+  fr: messagesFr,
+  en: messagesEn,
+};
+
+const DiscoverScanR = props => (
+  <IntlProvider locale={props.language} messages={messages[props.language]}>
+    <section style={sectionStyle} className={classes.DiscoverScanR}>
+      <div className="container">
+        <div className="row flex-column justify-content-around text-center text-white">
+          <FormattedHTMLMessage
+            id={`DiscoverScanR.${props.labelKey}`}
+            defaultMessage={`DiscoverScanR.${props.labelKey}`}
+          />
+          <ComponentVideo
+            url="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+            poster={`url(${Poster})`}
+          />
+        </div>
+      </div>
+    </section>
+  </IntlProvider>
+);
+
+export default DiscoverScanR;
+
+DiscoverScanR.propTypes = {
+  labelKey: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+};
