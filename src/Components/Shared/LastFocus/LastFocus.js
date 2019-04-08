bee8f560619df89bf9263ed9894885b7b5c017f8
jeremy.peglion <jeremy.peglion@gmail.com>
2019-04-08 13:38:43 +0200

    Mise en composants des SVG focus

--- src/Components/Shared/LastFocus/LastFocus.js
+++ src/Components/Shared/LastFocus/LastFocus.js
@@ -47,6 +47,7 @@ const LastFocus = (props) => {
               props.focusList.map(oneFocus => (
                 <div className="col-lg-4">
                   <FocusCard
+                    schema={oneFocus.schema}
                     tags={oneFocus.tags}
                     title={oneFocus.title}
                     type={oneFocus.type}
@@ -75,36 +76,42 @@ LastFocus.defaultProps = {
       title: 'La carte des poles de compétitivité en France',
       type: 'map',
       url: '',
+      schema: 'entity',
     },
     {
       tags: ['#tag1', '#tag2', '#tag3'],
       title: 'Présence sur le web: état des lieux des laboratoires français',
       type: 'treemap',
       url: '',
+      schema: 'person',
     },
     {
       tags: ['#tag1', '#tag2', '#tag3'],
       title: 'Japan Prize: Qui sont les lauréats français ?',
       type: 'histoTop',
       url: '',
+      schema: 'entity',
     },
     {
       tags: ['#tag1', '#tag2', '#tag3'],
       title: 'Dépots de brevets : Evolution des dernières années en France',
       type: 'bubbleTop',
       url: '',
+      schema: 'publication',
     },
     {
       tags: ['#tag1', '#tag2', '#tag3'],
       title: 'La carte des projets financés par l\'ANR',
       type: 'donut',
       url: '',
+      schema: 'project',
     },
     {
       tags: ['#tag1', '#tag2', '#tag3'],
       title: 'Quels sont les thèmes majeurs des thèses en France aujourd\'hui',
       type: 'bubble',
       url: '',
+      schema: 'entity',
     },
   ],
 };
