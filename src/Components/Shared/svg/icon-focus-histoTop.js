b1e7e2246252b944a2d0b9b1eeed4459075afb42
jeremy.peglion <jeremy.peglion@gmail.com>
2019-04-08 13:38:43 +0200

    Mise en composants des SVG focus

---
+++ src/Components/Shared/svg/icon-focus-histoTop.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SVG = props => (
+  <svg
+    width={props.width}
+    style={props.style}
+    height={props.width}
+    xmlns="http://www.w3.org/2000/svg"
+    className={`svg-icon ${props.className || ''}`}
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <g id="icon-focus-histoTop">
+      <g>
+        <g>
+          <g>
+            <path d="M16.25,22.75v-5.5H111a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M111,18.5a1.5,1.5,0,0,1,0,3H17.5v-3H111m0-2.5H15v8h96a4,4,0,0,0,0-8Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,32.75v-5.5H91a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M91,28.5a1.5,1.5,0,0,1,0,3H17.5v-3H91M91,26H15v8H91a4,4,0,0,0,0-8Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,42.75v-5.5H87a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M87,38.5a1.5,1.5,0,0,1,0,3H17.5v-3H87M87,36H15v8H87a4,4,0,0,0,0-8Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,52.75v-5.5H77a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M77,48.5a1.5,1.5,0,0,1,0,3H17.5v-3H77M77,46H15v8H77a4,4,0,0,0,0-8Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,62.75v-5.5H64a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M64,58.5a1.5,1.5,0,0,1,0,3H17.5v-3H64M64,56H15v8H64a4,4,0,0,0,0-8Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,72.75v-5.5H59a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M59,68.5a1.5,1.5,0,1,1,0,3H17.5v-3H59M59,66H15v8H59a4,4,0,0,0,4-4h0a4,4,0,0,0-4-4Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,82.75v-5.5H54a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M54,78.5a1.5,1.5,0,1,1,0,3H17.5v-3H54M54,76H15v8H54a4,4,0,0,0,4-4h0a4,4,0,0,0-4-4Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,92.75v-5.5H44a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M44,88.5a1.5,1.5,0,1,1,0,3H17.5v-3H44M44,86H15v8H44a4,4,0,0,0,4-4h0a4,4,0,0,0-4-4Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,102.75v-5.5H41a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M41,98.5a1.5,1.5,0,1,1,0,3H17.5v-3H41M41,96H15v8H41a4,4,0,0,0,4-4h0a4,4,0,0,0-4-4Z" fill="#fff" />
+          </g>
+          <g>
+            <path d="M16.25,112.75v-5.5H31a2.75,2.75,0,0,1,0,5.5Z" fill={props.fill} />
+            <path d="M31,108.5a1.5,1.5,0,1,1,0,3H17.5v-3H31m0-2.5H15v8H31a4,4,0,0,0,4-4h0a4,4,0,0,0-4-4Z" fill="#fff" />
+          </g>
+        </g>
+        <g>
+          <path d="M15,16h96a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V16A0,0,0,0,1,15,16Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,26H91a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V26A0,0,0,0,1,15,26Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,36H87a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V36A0,0,0,0,1,15,36Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,46H77a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V46A0,0,0,0,1,15,46Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,56H64a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V56A0,0,0,0,1,15,56Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,66H59a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V66A0,0,0,0,1,15,66Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,76H54a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V76A0,0,0,0,1,15,76Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,86H44a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V86A0,0,0,0,1,15,86Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,96H41a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0V96A0,0,0,0,1,15,96Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+          <path d="M15,106H31a4,4,0,0,1,4,4v0a4,4,0,0,1-4,4H15a0,0,0,0,1,0,0v-8A0,0,0,0,1,15,106Z" fill="none" stroke={props.fill} strokeMiterlimit="10" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+
+export default SVG;
+
+SVG.propTypes = {
+  style: PropTypes.object,
+  fill: PropTypes.string,
+  width: PropTypes.string,
+  className: PropTypes.string,
+};
+
+SVG.defaultProps = {
+  style: {},
+  fill: '#000',
+  width: '100%',
+  className: '',
+};
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,9 @@
 // import React from 'react';
 
 export const API_END_POINT = '//88.168.181.80:3000/api/v1/';
+
+// Couleurs du site
+export const ENTITY_COLOR = '#ebc034';
+export const PERSON_COLOR = '#fe7747';
+export const PROJECT_COLOR = '#5dd99d';
+export const PUBLICATION_COLOR = '#cc3d8f';
