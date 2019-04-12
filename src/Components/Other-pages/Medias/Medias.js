583594fab7b1689f887d99cf898a9b7c1813c723
magali <mogwy8@yahoo.fr>
2019-04-12 18:33:34 +0200

    nouvelle page media et création de mediacard

--- src/Components/Other-pages/Medias/Medias.js
+++ src/Components/Other-pages/Medias/Medias.js
@@ -6,7 +6,9 @@ import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header-homePage';
 import HeaderTitle from '../../Shared/HeaderTitle/HeaderTitle';
 import Banner from '../../Shared/Banner/Banner';
-import DiscoverThreeCards from '../../Shared/DiscoverThreeCards/DiscoverThreeCards';
+import Background from './poudre-bleu_Fgris-B.jpg';
+import CardWithButton from '../../Shared/CardWithButton/CardWithButton';
+import MediasCard from '../../Shared/CardWithButton/MediasCard';
 
 /* Gestion des langues */
 import messagesFr from './translations/fr.json';
@@ -20,6 +22,10 @@ const messages = {
   en: messagesEn,
 };
 
+const sectionStyle = {
+  backgroundImage: `url(${Background})`,
+};
+
 const Medias = props => (
   <IntlProvider locale={props.language} messages={messages[props.language]}>
     <div className={`container-fluid ${classes.Medias}`}>
@@ -27,23 +33,75 @@ const Medias = props => (
         language={props.language}
         switchLanguage={props.switchLanguage}
       />
-      <section>
-        <HeaderTitle
-          language={props.language}
-          label="media"
-        />
-        <DiscoverThreeCards
-          language={props.language}
-          TitleCard1="Discover.TalkAboutScanr"
-          TitleCard2="Discover.Sources"
-          TitleCard3="Discover.Team"
-        />
-        <Banner
-          bannerColor={classes.BannerColor}
-          label="TalkAboutScanr"
-          language={props.language}
-        />
+      <HeaderTitle
+        language={props.language}
+        label="media"
+      />
+      <section style={sectionStyle} className={classes.Content}>
+        <div className="container">
+          <div className={`row ${classes.periode}`}>
+            en 2019
+          </div>
+          <div className="row">
+            <MediasCard
+              language={props.language}
+              article="Medias.article1"
+            />
+          </div>
+          <div className={`row ${classes.periode}`}>
+            en 2018
+          </div>
+          <div className="row">
+            <MediasCard
+              language={props.language}
+              article="Medias.article2"
+            />
+            <MediasCard
+              language={props.language}
+              article="Medias.article3"
+            />
+            <MediasCard
+              language={props.language}
+              article="Medias.article4"
+            />
+          </div>
+        </div>
       </section>
+      <section className={classes.ThreeCards}>
+        <div className="container">
+          <div className="row">
+            <CardWithButton
+              language={props.language}
+              schema="card_dark"
+              title="Discover.TalkAboutScanr"
+              url="https://worldwide.espacenet.com/?locale=fr_EP"
+              lib_button="Découvrir"
+              position="CardCenter"
+            />
+            <CardWithButton
+              language={props.language}
+              schema="card_dark"
+              title="Discover.Sources"
+              url="https://worldwide.espacenet.com/?locale=fr_EP"
+              lib_button="Découvrir"
+              position="CardCenter"
+            />
+            <CardWithButton
+              language={props.language}
+              schema="card_dark"
+              title="Discover.Team"
+              url="https://worldwide.espacenet.com/?locale=fr_EP"
+              lib_button="Découvrir"
+              position="CardCenter"
+            />
+          </div>
+        </div>
+      </section>
+      <Banner
+        language={props.language}
+        label="Appear"
+        cssClass="BannerDark"
+      />
       <Footer language={props.language} />
     </div>
   </IntlProvider>
