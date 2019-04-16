a719d9b133ef6fceda50a56326775b4563caac7a
jeremy.peglion <jeremy.peglion@gmail.com>
2019-04-16 14:37:41 +0200

    Modification du composant pour qu'il accèpte les logos statics des structures + fix merge

--- src/Components/Shared/Ui/LogoCard/LogoCard.js
+++ src/Components/Shared/Ui/LogoCard/LogoCard.js
@@ -4,18 +4,23 @@ import PropTypes from 'prop-types';
 /* SCSS */
 import classes from './LogoCard.scss';
 
-const LogoCard = props => (
-  <div className={classes.card}>
-    <img
-      src={`./img/logo-${props.label}.svg`}
-      alt={props.label}
-      className={`img-fluid ${classes.img}`}
-    />
-  </div>
-);
+const LogoCard = (props) => {
+  const src = (props.src) ? props.src : `./img/logo-${props.label}.svg`;
+  return (
+    <div className={classes.card}>
+      <img
+        src={src}
+        alt={props.label}
+        className={`img-fluid ${classes.img}`}
+      />
+    </div>
+  );
+};
+
 
 export default LogoCard;
 
 LogoCard.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  src: PropTypes.string,
 };
