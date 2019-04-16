85b74e9206b264b8c50818260171d81c43c7dc89
jeremy.peglion <jeremy.peglion@gmail.com>
2019-04-16 17:24:54 +0200

    update

--- src/Components/Shared/Ui/SimpleListCard/SimpleListCard.js
+++ src/Components/Shared/Ui/SimpleListCard/SimpleListCard.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import ReactTooltip from 'react-tooltip';
 
 import classes from './SimpleListCard.scss';
 
@@ -15,12 +16,25 @@ const SimpleListCard = (props) => {
   const logo = (props.logo) ? <div className={classes.Logo}><i className={props.logo} /></div> : null;
   const title = (props.title) ? <div className={classes.Title}>{props.title}</div> : null;
   const label = (props.label) ? <div className={classes.Label}>{props.label}</div> : null;
+  const tooltip = (props.tooltip) ? (
+    <Fragment>
+      <span className={classes.Tooltip_i_top_right} data-tip={props.tooltip}>i</span>
+      <ReactTooltip html />
+    </Fragment>
+  ) : null;
+
+  let items = null;
+  if (props.list) {
+    items = props.list.map(item => <li>{item.value}</li>);
+  }
 
   return (
     <div className={classes.SimpleListCard}>
       {logo}
       {title}
       {label}
+      {tooltip}
+      {(items) || null}
     </div>
   );
 };
