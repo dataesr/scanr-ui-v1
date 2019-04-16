13b2d0dd339d45498e2496d15f5291c45acb4705
miarkt <rakoto.mk@gmail.com>
2019-04-16 20:01:37 +0200

    modif breadcrumb.js

--- src/Components/Shared/Ui/Breadcrumb/Breadcrumb.js
+++ src/Components/Shared/Ui/Breadcrumb/Breadcrumb.js
@@ -7,7 +7,6 @@ const BreadCrumb = () => (
   <nav className={classes.BreadCrumb} aria-label="breadcrumb">
     <ol className={`breadcrumb ${classes.breadcrumb}`}>
       <li className={`breadcrumb-item ${classes.item}`}>Accueil</li>
-      <li className={`breadcrumb-item ${classes.item}`}>test</li>
       <li className={`breadcrumb-item active ${classes.active}`} aria-current="page">Mentions légales</li>
     </ol>
   </nav>
