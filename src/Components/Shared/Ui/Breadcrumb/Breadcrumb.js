775b713b09428d3cb46f06dbf3ed9919d27d2206
miarkt <rakoto.mk@gmail.com>
2019-04-16 18:19:21 +0200

    position logo ministère

--- src/Components/Shared/Ui/Breadcrumb/Breadcrumb.js
+++ src/Components/Shared/Ui/Breadcrumb/Breadcrumb.js
@@ -6,7 +6,8 @@ import classes from './Breadcrumb.scss';
 const BreadCrumb = () => (
   <nav className={classes.BreadCrumb} aria-label="breadcrumb">
     <ol className={`breadcrumb ${classes.breadcrumb}`}>
-      <li className="breadcrumb-item">Accueil</li>
+      <li className={`breadcrumb-item ${classes.item}`}>Accueil</li>
+      <li className={`breadcrumb-item ${classes.item}`}>test</li>
       <li className={`breadcrumb-item active ${classes.active}`} aria-current="page">Mentions légales</li>
     </ol>
   </nav>
