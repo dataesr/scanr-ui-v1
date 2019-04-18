bc69b828be4b7b715d55c302bb1320d47e9d1f8d
Jerem <jeremy.peglion@gmail.com>
2019-04-18 15:16:16 +0200

    Ajout de la section Histoire

---
+++ src/Components/Results/Entity-page/Portrait/History/History.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import CardsTitle from '../../../../Shared/Ui/CardsTitle/CardsTitle';
+import SimpleCard from '../../../../Shared/Ui/SimpleCard/SimpleCard';
+
+import classes from './History.scss';
+
+/* Gestion des langues */
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
+
+
+/**
+ * History
+ * Url : ex: /entite/200711886U
+ * Description : Bloc identité visible dans la section Protrait
+ * Responsive : .
+ * Accessible : .
+ * Tests unitaires : .
+*/
+const messages = {
+  fr: messagesFr,
+  en: messagesEn,
+};
+
+const History = (props) => {
+  return (
+    <div className={classes.Identity}>
+      <div className="row">
+        <div className={`col ${classes.NoSpace}`}>
+          <CardsTitle title={messages[props.language]['Entity.portrait.history.title']} />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className={`col-6 ${classes.NoSpace}`}>
+          <SimpleCard
+            logo="fas fa-id-card"
+            title={messages[props.language]['Entity.portrait.history.createdDate']}
+            label={props.creationYear}
+            tooltip=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default History;
+
+History.propTypes = {
+  language: PropTypes.string.isRequired,
+  acronym: PropTypes.array,
+  externalIds: PropTypes.array,
+  id: PropTypes.string,
+  name: PropTypes.object,
+  nature: PropTypes.string,
+};
