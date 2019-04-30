d92731f4c82255214f4d8af7b5c6571970e036e7
Tanguy D'auria <tanguy.d-auria@epitech.eu>
2019-04-30 14:09:02 +0100

    Renamed custom scss file + fixed colors on leaflet map

--- src/Components/Focus-pages/focus-1/MarkerStyle.js
+++ src/Components/Focus-pages/focus-1/MarkerStyle.js
@@ -1,7 +1,7 @@
 import * as L from 'leaflet';
 
 const yellowIcon = new L.Icon({
-  iconUrl: 'marker-icon-2x-scanr.png',
+  iconUrl: 'img/marker-icon-2x-scanr.png',
   // iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [25, 41],
