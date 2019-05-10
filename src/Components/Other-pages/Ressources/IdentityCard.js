3f683a6e2e8c3a609f0f31e3ad1feecf0bf81793
magali <mogwy8@yahoo.fr>
2019-05-10 17:55:31 +0200

    identity card de la page ressource et création du fichier json metadata pour mettre les adresses de sites web des ressources

--- src/Components/Other-pages/Ressources/IdentityCard.js
+++ src/Components/Other-pages/Ressources/IdentityCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FormattedHTMLMessage } from 'react-intl';
 import PropTypes from 'prop-types';
+import ButtonToPage from '../../Shared/Ui/Buttons/ButtonToPage';
 
 
 /* SCSS */
@@ -9,15 +10,62 @@ import classes from './IdentityCard.scss';
 const IdentityCard = (props) => {
   const logo = `../img/logo-${props.labelKey}.svg`;
   return (
-    <div className={classes.IdentityCard}>
-      <div className={classes.Logo}>
+    <div className={`container ${classes.IdentityCard}`}>
+      <div className={`row ${classes.Logo}`}>
         <img
           src={logo}
           alt={props.labelKey}
           className={`img-fluid ${classes.img}`}
         />
       </div>
-      <hr className={classes.Demarcation} />
+      <hr />
+      <a href={props.WebSource} target="_blank" rel="noopener noreferrer">
+        <div className={`row ${classes.LienSiteExterne}`}>
+          <div className={classes.SiteExterne}>
+          Site internet de la source
+          </div>
+          <span className="col text-right">
+            <i className="fas fa-arrow-right" />
+          </span>
+        </div>
+      </a>
+      <hr />
+      <div className={classes.ProducteurGras}>
+      Producteur
+      </div>
+      <div className={classes.ProducteurNom}>
+        <FormattedHTMLMessage
+          id={`${props.labelKey}.Producteur`}
+          defaultMessage="s"
+        />
+      </div>
+      <a href={props.WebProducteur} target="_blank" rel="noopener noreferrer">
+        <div className={`row ${classes.LienSiteExterne}`}>
+          <div className={classes.SiteExterne}>
+            Site internet
+          </div>
+          <span className="col text-right">
+            <i className="fas fa-arrow-right" />
+          </span>
+        </div>
+      </a>
+      <a href={props.WebWikipedia} target="_blank" rel="noopener noreferrer">
+        <div className={`row ${classes.LienSiteExterne}`}>
+          <div className={classes.SiteExterne}>
+            Page Wikipédia
+          </div>
+          <span className="col text-right">
+            <i className="fas fa-arrow-right" />
+          </span>
+        </div>
+      </a>
+      <hr />
+      <ButtonToPage
+        className={classes.Button}
+        url=""
+      >
+        Voir dans scanR
+      </ButtonToPage>
     </div>
   );
 };
@@ -27,4 +75,5 @@ export default IdentityCard;
 
 IdentityCard.propTypes = {
   labelKey: PropTypes.string,
+  webSite: PropTypes.string,
 };
