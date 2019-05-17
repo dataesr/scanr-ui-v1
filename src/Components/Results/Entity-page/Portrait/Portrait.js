f5f9a5fd0e355c9d2391993a3ab8da8169f6d768
jeremy.peglion <jeremy.peglion@gmail.com>
2019-05-17 10:46:02 +0200

    update

--- src/Components/Results/Entity-page/Portrait/Portrait.js
+++ src/Components/Results/Entity-page/Portrait/Portrait.js
@@ -1,10 +1,12 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import History from './History/History';
 import Identity from './Identity/Identity';
 import Localisation from './Localisation/Localisation';
 import SectionTitle from '../../../Shared/Results/SectionTitle/SectionTitle';
+import Web from './Web/Web';
+
 import Background from '../../../Shared/images/poudre-jaune_Fgris-B.jpg';
 
 import classes from './Portrait.scss';
@@ -18,69 +20,65 @@ import classes from './Portrait.scss';
  * Tests unitaires : .
 */
 
-class Portrait extends Component {
-  componentDidMount() {
-    console.log('componentDidMount()');
+const Portrait = (props) => {
+  if (!props.data) {
+    return null;
   }
+  const sectionStyle = {
+    backgroundImage: `url(${Background})`,
+  };
 
-  render() {
-    if (!this.props.data) {
-      return null;
-    }
-    const sectionStyle = {
-      backgroundImage: `url(${Background})`,
-    };
-
-    return (
-      <Fragment>
-        <section className={`container-fluid ${classes.Portrait}`} style={sectionStyle}>
-          <div className="container">
-            <SectionTitle icon="fas fa-id-card">Portrait</SectionTitle>
-            <div className="row">
-              <div className="col-6">
-                <Identity
-                  acronym={this.props.data.acronym}
-                  externalIds={[{ key: 'siren', value: 'gdklsjg4' }, { key: 'uai', value: '123456' }]}
-                  id={this.props.data.id}
-                  language={this.props.language}
-                  name={this.props.data.label}
-                  nature={this.props.data.nature}
-                />
-              </div>
-              <div className={`col-6 ${classes.NoSpace}`}>
-                <Localisation
-                  address={this.props.data.address}
-                  language={this.props.language}
-                />
-              </div>
-              <div className="col-6">
-                3-Domaines d expertise
-              </div>
-              <div className="col-6">
-                4-Direction
-              </div>
-              <div className="col-6">
-                <History
-                  creationYear={this.props.data.creationYear}
-                  id={this.props.data.id}
-                  language={this.props.language}
-                  predecessors={this.props.data.predecessors}
-                />
-              </div>
+  return (
+    <Fragment>
+      <section className={`container-fluid ${classes.Portrait}`} style={sectionStyle}>
+        <div className="container">
+          <SectionTitle icon="fas fa-id-card">Portrait</SectionTitle>
+          <div className="row">
+            <div className="col-6">
+              <Identity
+                acronym={props.data.acronym}
+                externalIds={[{ key: 'siren', value: 'gdklsjg4' }, { key: 'uai', value: '123456' }]}
+                id={props.data.id}
+                language={props.language}
+                name={props.data.label}
+                nature={props.data.nature}
+              />
             </div>
-            <div className="row">
-              <div className="col-12">Présence sur le web</div>
-              <div className="col-3">Sites</div>
-              <div className="col-3">Infos extraites</div>
-              <div className="col-3">wiki</div>
-              <div className="col-3">Rapport HCERES</div>
+            <div className={`col-6 ${classes.NoSpace}`}>
+              <Localisation
+                address={props.data.address}
+                language={props.language}
+              />
+            </div>
+            <div className="col-6">
+              3-Domaines d expertise
+            </div>
+            <div className="col-6">
+              4-Direction
+            </div>
+            <div className="col-6">
+              <History
+                creationYear={props.data.creationYear}
+                id={props.data.id}
+                language={props.language}
+                predecessors={props.data.predecessors}
+              />
+            </div>
+
+            <div className="col-12">
+              <Web
+                id={props.data.id}
+                language={props.language}
+                socialMedias={props.data.socialMedias}
+                websites={props.data.websites}
+              />
             </div>
           </div>
-        </section>
-      </Fragment>
-    );
-  }
-}
+        </div>
+      </section>
+    </Fragment>
+  );
+};
 
 export default Portrait;
 
