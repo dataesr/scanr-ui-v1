d2fcc9dfe63bc9eac2357c267c917a564b722d44
Tanguy D'auria <tanguy.d-auria@epitech.eu>
2019-05-21 15:12:18 +0100

    Added share and export functions to HighChartsBar + added icons for each component

--- src/Components/Focus-pages/focus-1/graphs/HighChartsBar.js
+++ src/Components/Focus-pages/focus-1/graphs/HighChartsBar.js
@@ -1,96 +1,131 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import HCRounded from 'highcharts-rounded-corners';
 import HCExporting from 'highcharts/modules/exporting';
 import HCExportingData from 'highcharts/modules/export-data';
+import HCRounded from 'highcharts-rounded-corners';
 
 HCExporting(Highcharts);
 HCExportingData(Highcharts);
 HCRounded(Highcharts);
 
-const options = {
-  chart: {
-    type: 'bar',
-  },
-  credits: {
-    enabled: false,
-  },
-  // labels: {
-  //     align: 'right',
-  //     x: -1,
-  //     y: 0
-  // },
-  title: {
-    text: '',
-  },
-  xAxis: {
-    lineWidth: 0,
-    categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas'],
-    labels:
-    {
-      style: { color: '#000000' },
-      // enabled: false
-      align: 'left',
-      x: 20,
-    },
-  },
-  yAxis: {
-    min: 0,
-    gridLineWidth: 0,
-    minorGridLineWidth: 0,
-    title: {
-      text: '',
-      // text: 'Total fruit consumption'
-    },
-    labels:
-    {
-      enabled: false,
-    },
-  },
-  legend: {
-    hide: true,
-    enabled: false,
-    // reversed: true
-  },
-  plotOptions: {
-    series: {
-      stacking: 'normal',
-      pointPadding: 0,
-      // groupPadding: 0.1,
-      dataLabels: {
-        enabled: true,
-        align: 'right',
-        x: 15,
-        style: { color: '#000000' },
+export default class HighChartsBar extends Component {
+  constructor(props) {
+    super(props);
+    this.chart = React.createRef();
+    this.state = {
+      options: null,
+      data: this.props.data,
+    };
+    this.exportChartPdf = this.exportChartPdf.bind(this);
+    this.exportChartPng = this.exportChartPng.bind(this);
+    this.exportChartCsv = this.exportChartCsv.bind(this);
+  }
+
+  componentWillMount() {
+    this.state.options = {
+      chart: {
+        type: 'bar',
       },
-    },
-  },
-  series: [{
-    color: '#FDD85E',
-    name: 'John',
-    data: [7, 5, 4, 3, 2],
-    borderRadiusTopLeft: '80%',
-    borderRadiusTopRight: '80%',
-    borderRadiusBottomLeft: '80%',
-    borderRadiusBottomRight: '80%',
-  }],
-  // }, {
-  //   name: 'Jane',
-  //   data: [2, 2, 3, 2, 1],
-  // }, {
-  //   name: 'Joe',
-  //   data: [3, 4, 4, 2, 5],
-  // }],
+      credits: {
+        enabled: false,
+      },
+      title: {
+        text: '',
+      },
+      xAxis: {
+        lineWidth: 0,
+        categories: this.state.data.labels,
+        labels:
+        {
+          style: { color: '#000000' },
+          // enabled: false
+          align: 'left',
+          x: 20,
+        },
+      },
+      yAxis: {
+        min: 0,
+        gridLineWidth: 0,
+        minorGridLineWidth: 0,
+        title: { text: '' },
+        labels: { enabled: false },
+      },
+      legend: {
+        hide: true,
+        enabled: false,
+        // reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          pointPadding: 0,
+          // groupPadding: 0.1,
+          dataLabels: {
+            enabled: true,
+            align: 'right',
+            x: 15,
+            style: { color: '#000000' },
+          },
+        },
+      },
+      series: [{
+        color: '#FDD85E',
+        name: this.state.data.unit,
+        data: this.state.data.values,
+        borderRadiusTopLeft: '80%',
+        borderRadiusTopRight: '80%',
+        borderRadiusBottomLeft: '80%',
+        borderRadiusBottomRight: '80%',
+      }],
+      exporting: {
+        filename: this.props.filename,
+        buttons: {
+          contextButton: {
+            enabled: false,
+          },
+        },
+      },
+    };
+  }
+
+  exportChartPdf() {
+    this.chart.current.chart.exportChart({
+      type: 'application/pdf',
+    });
+  }
+
+  exportChartPng() {
+    this.chart.current.chart.exportChart({
+      type: 'image/png',
+    });
+  }
+
+  exportChartCsv() {
+    alert(this.chart.current.chart.getCSV());
+    alert(this.chart.current.chart.getTable());
+    alert(this.chart.current.chart.getDataRows());
+    this.chart.current.chart.getCSV();
+  }
+
+  render() {
+    return (
+      <div>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={this.state.options}
+          ref={this.chart}
+        />
+        <button type="button" onClick={this.exportChartPdf}>Export PDF</button>
+        <button type="button" onClick={this.exportChartPng}>Export PNG</button>
+        <button type="button" onClick={this.exportChartCsv}>Export CSV</button>
+      </div>
+    );
+  }
+}
+
+HighChartsBar.propTypes = {
+  filename: PropTypes.string.isRequired,
+  data: PropTypes.object.isRequired,
 };
-
-const HighChartsBar = () => (
-  <div>
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={options}
-    />
-  </div>
-);
-
-export default HighChartsBar;
