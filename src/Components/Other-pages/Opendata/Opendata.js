bf27da153590b4a4e7c085982a2633f7010a7b30
jeremy.peglion <jeremy.peglion@gmail.com>
2019-05-21 16:46:45 +0200

    update

--- src/Components/Other-pages/Opendata/Opendata.js
+++ src/Components/Other-pages/Opendata/Opendata.js
@@ -25,199 +25,209 @@ const messages = {
   en: messagesEn,
 };
 
-const Opendata = props => (
-  <IntlProvider locale={props.language} messages={messages[props.language]}>
-    <div className={`container-fluid ${classes.Opendata}`}>
-      <Header
-        language={props.language}
-        switchLanguage={props.switchLanguage}
-      />
-      <HeaderTitle
-        language={props.language}
-        label="opendata"
-      />
-      <section style={sectionStyle} className={classes.Content}>
-        <div className="container">
-          <div className="row" style={{ padding: '0px' }}>
-            <CardTwoColumns
-              language={props.language}
-              title="Opendata.TitleOpendata"
-            >
-              <FormattedHTMLMessage
-                id="Opendata.TitleOpendata.content"
-                defaultMessage="Opendata.TitleOpendata.content"
-              />
-            </CardTwoColumns>
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card01"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card02"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-          </div>
-          <div className="row">
-            <CardWithButton
-              language={props.language}
-              schema="projectsCards"
-              title="Opendata.card03"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="projectsCards"
-              title="Opendata.card04"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card05"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="personsCards"
-              title="Opendata.card06"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-          </div>
-          <div className="row">
-            <CardWithButton
-              language={props.language}
-              schema="personsCards"
-              title="Opendata.card07"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card08"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card09"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="entitiesCards"
-              title="Opendata.card10"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Voir"
-            />
-          </div>
-          <div className="row">
-            <div className="col-lg-3" style={{ padding: '0px' }}>
+const Opendata = (props) => {
+  if (props.language !== props.match.params.language) {
+    if (props.language === 'fr') {
+      props.history.push(props.location.pathname.replace('/en/', '/fr/'));
+    } else {
+      props.history.push(props.location.pathname.replace('/fr/', '/en/'));
+    }
+  }
+
+  return(
+    <IntlProvider locale={props.match.params.language} messages={messages[props.match.params.language]}>
+      <div className={`container-fluid ${classes.Opendata}`}>
+        <Header
+          language={props.match.params.language}
+          switchLanguage={props.switchLanguage}
+          />
+        <HeaderTitle
+          language={props.match.params.language}
+          labelkey="opendata"
+          />
+        <section style={sectionStyle} className={classes.Content}>
+          <div className="container">
+            <div className="row" style={{ padding: '0px' }}>
+              <CardTwoColumns
+                language={props.match.params.language}
+                title="Opendata.TitleOpendata"
+                >
+                <FormattedHTMLMessage
+                  id="Opendata.TitleOpendata.content"
+                  defaultMessage="Opendata.TitleOpendata.content"
+                  />
+              </CardTwoColumns>
               <CardWithButton
-                language={props.language}
-                schema="personsCards"
-                title="Opendata.card11"
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card01"
                 url="https://worldwide.espacenet.com/?locale=fr_EP"
                 lib_button="Voir"
-              />
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card02"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+            </div>
+            <div className="row">
+              <CardWithButton
+                language={props.match.params.language}
+                schema="projectsCards"
+                title="Opendata.card03"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="projectsCards"
+                title="Opendata.card04"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card05"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="personsCards"
+                title="Opendata.card06"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+            </div>
+            <div className="row">
+              <CardWithButton
+                language={props.match.params.language}
+                schema="personsCards"
+                title="Opendata.card07"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card08"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card09"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="entitiesCards"
+                title="Opendata.card10"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Voir"
+                />
+            </div>
+            <div className="row">
+              <div className="col-lg-3" style={{ padding: '0px' }}>
+                <CardWithButton
+                  language={props.match.params.language}
+                  schema="personsCards"
+                  title="Opendata.card11"
+                  url="https://worldwide.espacenet.com/?locale=fr_EP"
+                  lib_button="Voir"
+                  />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section className={classes.Plateforme}>
+        </section>
+        <section className={classes.Plateforme}>
+          <Banner
+            language={props.match.params.language}
+            labelKey="Opendata"
+            cssClass="BannerLight"
+            url="https://data.enseignementsup-recherche.gouv.fr/pages/home/"
+            target="_blank"
+            />
+        </section>
+        <section className={classes.Api}>
+          <div className="container">
+            <div className="row">
+              <CardTwoColumns
+                language={props.match.params.language}
+                title="Opendata.ApiScanr"
+                >
+                <FormattedHTMLMessage
+                  id="Opendata.ApiScanr.content"
+                  defaultMessage="Opendata.ApiScanr.content"
+                  />
+              </CardTwoColumns>
+              <CardWithButton
+                language={props.match.params.language}
+                title="Opendata.DocuApi"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Accéder"
+                schema="card_dark"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                title="Opendata.EnSavoirPlusApi"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Lire"
+                schema="card_dark"
+                />
+            </div>
+          </div>
+        </section>
+        <section className={classes.ThreeCards}>
+          <div className="container">
+            <div className="row">
+              <CardWithButton
+                language={props.match.params.language}
+                schema="card_dark"
+                title="Discover.TalkAboutScanr"
+                url="./medias"
+                lib_button="Découvrir"
+                position="CardCenter"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="card_dark"
+                title="Discover.Sources"
+                url="https://worldwide.espacenet.com/?locale=fr_EP"
+                lib_button="Découvrir"
+                position="CardCenter"
+                />
+              <CardWithButton
+                language={props.match.params.language}
+                schema="card_dark"
+                title="Discover.Team"
+                url="./l-equipe-et-son-projet"
+                lib_button="Découvrir"
+                position="CardCenter"
+                />
+            </div>
+          </div>
+        </section>
         <Banner
-          language={props.language}
-          labelKey="Opendata"
-          cssClass="BannerLight"
-          url="https://data.enseignementsup-recherche.gouv.fr/pages/home/"
-          target="_blank"
-        />
-      </section>
-      <section className={classes.Api}>
-        <div className="container">
-          <div className="row">
-            <CardTwoColumns
-              language={props.language}
-              title="Opendata.ApiScanr"
-            >
-              <FormattedHTMLMessage
-                id="Opendata.ApiScanr.content"
-                defaultMessage="Opendata.ApiScanr.content"
-              />
-            </CardTwoColumns>
-            <CardWithButton
-              language={props.language}
-              title="Opendata.DocuApi"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Accéder"
-              schema="card_dark"
-            />
-            <CardWithButton
-              language={props.language}
-              title="Opendata.EnSavoirPlusApi"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Lire"
-              schema="card_dark"
-            />
-          </div>
-        </div>
-      </section>
-      <section className={classes.ThreeCards}>
-        <div className="container">
-          <div className="row">
-            <CardWithButton
-              language={props.language}
-              schema="card_dark"
-              title="Discover.TalkAboutScanr"
-              url="./medias"
-              lib_button="Découvrir"
-              position="CardCenter"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="card_dark"
-              title="Discover.Sources"
-              url="https://worldwide.espacenet.com/?locale=fr_EP"
-              lib_button="Découvrir"
-              position="CardCenter"
-            />
-            <CardWithButton
-              language={props.language}
-              schema="card_dark"
-              title="Discover.Team"
-              url="./l-equipe-et-son-projet"
-              lib_button="Découvrir"
-              position="CardCenter"
-            />
-          </div>
-        </div>
-      </section>
-      <Banner
-        language={props.language}
-        labelKey="Appear"
-        cssClass="BannerDark"
-        url=""
-      />
-      <Footer language={props.language} />
-    </div>
-  </IntlProvider>
-);
+          language={props.match.params.language}
+          labelKey="Appear"
+          cssClass="BannerDark"
+          url=""
+          />
+        <Footer language={props.match.params.language} />
+      </div>
+    </IntlProvider>
+  );
+};
 
 export default Opendata;
 
 Opendata.propTypes = {
-  language: PropTypes.string.isRequired,
   switchLanguage: PropTypes.func.isRequired,
+  match: PropTypes.object,
 };
--- src/Components/Results/Entity-page/Portrait/Portrait.js
+++ src/Components/Results/Entity-page/Portrait/Portrait.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+import ExpertiseField from './ExpertiseField/ExpertiseField';
 import History from './History/History';
 import Identity from './Identity/Identity';
+import Leaders from './Leaders/Leaders';
 import Localisation from './Localisation/Localisation';
 import SectionTitle from '../../../Shared/Results/SectionTitle/SectionTitle';
 import Web from './Web/Web';
@@ -34,45 +36,63 @@ const Portrait = (props) => {
         <div className="container">
           <SectionTitle icon="fas fa-id-card">Portrait</SectionTitle>
           <div className="row">
-            <div className="col-6">
-              <Identity
-                acronym={props.data.acronym}
-                externalIds={[{ key: 'siren', value: 'gdklsjg4' }, { key: 'uai', value: '123456' }]}
-                id={props.data.id}
-                language={props.language}
-                name={props.data.label}
-                nature={props.data.nature}
-              />
-            </div>
-            <div className={`col-6 ${classes.NoSpace}`}>
-              <Localisation
-                address={props.data.address}
-                language={props.language}
-              />
-            </div>
-            <div className="col-6">
-              3-Domaines d expertise
-            </div>
-            <div className="col-6">
-              4-Direction
-            </div>
-            <div className="col-6">
-              <History
-                creationYear={props.data.creationYear}
-                id={props.data.id}
-                language={props.language}
-                predecessors={props.data.predecessors}
-              />
-            </div>
+            <Identity
+              acronym={props.data.acronym}
+              externalIds={[{ key: 'siren', value: 'gdklsjg4' }, { key: 'uai', value: '123456' }]}
+              id={props.data.id}
+              language={props.language}
+              name={props.data.label}
+              nature={props.data.nature}
+            />
 
-            <div className="col-12">
-              <Web
-                id={props.data.id}
-                language={props.language}
-                socialMedias={props.data.socialMedias}
-                websites={props.data.websites}
-              />
-            </div>
+            <Localisation
+              address={props.data.address}
+              language={props.language}
+            />
+
+            <History
+              creationYear={props.data.creationYear}
+              id={props.data.id}
+              language={props.language}
+              predecessors={props.data.predecessors}
+            />
+
+            <Leaders
+              id={props.data.id}
+              language={props.language}
+              leaders={props.data.leaders}
+            />
+
+            <History
+              creationYear={props.data.creationYear}
+              id={props.data.id}
+              language={props.language}
+              predecessors={props.data.predecessors}
+            />
+
+            <History
+              creationYear={props.data.creationYear}
+              id={props.data.id}
+              language={props.language}
+              predecessors={props.data.predecessors}
+            />
+
+            <ExpertiseField
+              id={props.data.id}
+              language={props.language}
+            />
+
+
+
+
+
+            <Web
+              id={props.data.id}
+              language={props.language}
+              socialMedias={props.data.socialMedias}
+              websites={props.data.websites}
+            />
+
           </div>
         </div>
       </section>
