b7749290c006e404753a386f3c6e4374726fc9a8
jeremy.peglion <jeremy.peglion@gmail.com>
2019-05-24 11:22:55 +0200

    fix opoendata url

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ class App extends Component {
             />
 
             <Route
-              path="/:language/opendata"
+              path="/opendata"
               component={props => (
                 <Opendata
                   {...props}
