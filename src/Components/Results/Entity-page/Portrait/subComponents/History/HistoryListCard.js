d483a768ceec68be082bf0e97b42535de38de93c
jeremy.peglion <jeremy.peglion@gmail.com>
2019-06-04 12:05:06 +0200

    avancement détail entité

---
+++ src/Components/Results/Entity-page/Portrait/subComponents/History/HistoryListCard.js
@@ -0,0 +1,114 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import ReactTooltip from 'react-tooltip';
+import ButtonWithModal from '../../../../../Shared/Ui/Buttons/ButtonWithModal';
+
+import classes from './History.scss';
+
+/**
+ * HistoryListCard component
+ * Url : .
+ * Description : Carte avec logo, titre, label, tooltip et bouton qui ouvre une modale affichant une liste d'items trié par année et par type
+ * Responsive : .
+ * Accessible : .
+ * Tests unitaires : .
+*/
+
+const titleFunction = title => (
+  (title) ? <div className={classes.Title}>{title}</div> : null
+);
+
+const additionalListFunction = (allProps) => {
+  // sorting by years
+  const listByYear = allProps.list.sort((a, b) => (a.eventYear - b.eventYear));
+
+  const objFinal = [];
+  const objYear = {};
+  let year = '';
+  for (let i = 0; i < listByYear.length; i += 1) {
+    if (listByYear[i].eventYear !== year) {
+      year = listByYear[i].eventYear;
+      objYear.year = year;
+      objYear.data = [];
+      objFinal.push(objYear);
+    }
+
+    const data = {};
+    data.type = listByYear[i].eventType;
+    data.id = listByYear[i].structure.id;
+    data.label = listByYear[i].structure.label.fr;
+    objYear.data.push(data);
+  }
+
+  // sorting by types
+  // TODO si pas fait par API
+
+  const itemsHtml = objFinal.map((itemYear) => {
+    let title = '';
+    const dataHtml = itemYear.data.map((item) => {
+      title = `${itemYear.year} - ${item.type}`;
+      return (
+        <div className="row">
+          <div className="col">
+            {item.id}
+          </div>
+          <div className="col">
+            {item.label}
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h2>{title}</h2>
+        {dataHtml}
+      </div>
+    );
+  });
+
+  // const items = <li>toto</li>;
+  // const itemsHtml = <ul className="list-group list-group-flush">{items}</ul>;
+  return (
+    <ButtonWithModal
+      logo={allProps.logo}
+      title={allProps.title}
+      buttonLabel={allProps.labelListButton}
+      dataHtml={itemsHtml}
+    />
+  );
+};
+
+const HistoryListCard = (props) => {
+  if (!props.list) {
+    return null;
+  }
+
+  const tooltip = (props.tooltip) ? (
+    <Fragment>
+      <span className={classes.Tooltip_i_top_right} data-tip={props.tooltip}>i</span>
+      <ReactTooltip html />
+    </Fragment>
+  ) : null;
+
+  return (
+    <div className={classes.HistoryListCard}>
+      <div className={classes.Logo}>
+        <i className="fas fa-clock" />
+      </div>
+
+      {titleFunction(props.title)}
+
+      {tooltip}
+
+      {additionalListFunction(props)}
+    </div>
+  );
+};
+
+export default HistoryListCard;
+
+HistoryListCard.propTypes = {
+  list: PropTypes.array,
+  title: PropTypes.string,
+  tooltip: PropTypes.string,
+};
--- src/Components/Shared/Ui/PersonCard/PersonCard.js
+++ src/Components/Shared/Ui/PersonCard/PersonCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
 import classes from './PersonCard.scss';
 
 import logo from '../../svg/icon-fiche-responsable_h.svg';
 import ButtonToPage from '../Buttons/ButtonToPage';
+
+
+/* Gestion des langues */
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
 
 /**
  * Leaders
@@ -14,62 +20,101 @@ import ButtonToPage from '../Buttons/ButtonToPage';
  * Accessible : .
  * Tests unitaires : .
 */
+const PersonCard = (props) => {
+  const messages = {
+    fr: messagesFr,
+    en: messagesEn,
+  };
 
-const PersonCard = props => (
-  <div className={classes.PersonCard}>
-    <div className="row">
-      <div className="col text-center">
-        <span className={classes.Title}>Responsable</span>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col text-center">
-        <div className={classes.Logo}>
-          <img src={logo} alt="logo" />
+  let email = props.email;
+  let firstName = props.firstName;
+  let lastName = props.lastName;
+  const role = props.role;
+  let url = null;
+
+  if (props.data.person) {
+    if (props.data.person.email) {
+      email = props.data.person.email;
+    }
+    if (props.data.person.firstName) {
+      firstName = props.data.person.firstName;
+    }
+    if (props.data.person.lastName) {
+      lastName = props.data.person.lastName;
+    }
+    if (props.data.person.id) {
+      url = `/person/${props.data.person.id}`;
+    }
+  }
+
+  return (
+    <IntlProvider locale={props.language} messages={messages[props.language]}>
+      <div className={`d-flex flex-column ${classes.PersonCard}`}>
+        <div>
+          <span className={classes.Title}>
+            <FormattedHTMLMessage
+              id="PersonCard.title"
+              defaultMessage="PersonCard.title"
+            />
+          </span>
         </div>
+        <div>
+          <div className={classes.Logo}>
+            <img src={logo} alt="logo" />
+          </div>
+        </div>
+        <div>
+          <span className={classes.Name}>
+            {`${firstName} ${lastName}`}
+          </span>
+        </div>
+        <div>
+          {(role) ? <span className={classes.Role}>{role}</span> : null}
+        </div>
+        <div>
+          {
+            (email)
+              ? (
+                <span className={classes.Email}>
+                  <a href={`"mailto:${props.email}"`}>
+                    <FormattedHTMLMessage
+                      id="PersonCard.emailLink.label"
+                      defaultMessage="PersonCard.emailLink.label"
+                    />
+                  </a>
+                </span>)
+              : null
+          }
+        </div>
+        {
+          (url)
+            ? (
+              <div className="mt-auto">
+                <ButtonToPage
+                  className={`${classes.MarginTop} ${classes.Button} ${classes.Component_dark}`}
+                  url={url}
+                >
+                  <FormattedHTMLMessage
+                    id="PersonCard.ButtonToPage.label"
+                    defaultMessage="PersonCard.ButtonToPage.label"
+                  />
+                </ButtonToPage>
+              </div>
+            )
+            : null
+        }
       </div>
-    </div>
-    <div className="row">
-      <div className="col text-center">
-        <span className={classes.Name}>{props.name}</span>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col text-center">
-        <span className={classes.Role}>{props.role}</span>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col text-center">
-        <span className={classes.Email}>
-          <a href={`"mailto:${props.email}"`}>
-            Lui envoyer un email
-          </a>
-        </span>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col text-center">
-        <ButtonToPage
-          className={`${classes.MarginTop} ${classes.Button} ${classes.Component_dark}`}
-          url=""
-        >
-          Sa fiche scanR
-        </ButtonToPage>
-      </div>
-    </div>
-  </div>
-);
+    </IntlProvider>
+  );
+};
 
 export default PersonCard;
-PersonCard.defaultProps = {
-  name: 'Jérémy PEGLION',
-  email: 'jeremy.peglion@gmail.com',
-  role: 'Directeur',
-};
+
 PersonCard.propTypes = {
-  name: PropTypes.string,
+  data: PropTypes.object,
   email: PropTypes.string,
-  role: PropTypes.string,
+  firstName: PropTypes.string,
   language: PropTypes.string,
+  lastName: PropTypes.string,
+  role: PropTypes.string,
 };
