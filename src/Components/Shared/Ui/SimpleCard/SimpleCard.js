40ebccc3b7be7317f4085121e803540957001af7
jeremy.peglion <jeremy.peglion@gmail.com>
2019-09-02 09:34:15 +0200

    work from last week

--- src/Components/Shared/Ui/SimpleCard/SimpleCard.js
+++ src/Components/Shared/Ui/SimpleCard/SimpleCard.js
@@ -2,6 +2,10 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
 
+import getSelectKey from '../../../../Utils/getSelectKey';
+
+import SubmitBox from '../../SubmitBox/SubmitBox';
+
 import classes from './SimpleCard.scss';
 
 /**
@@ -25,6 +29,7 @@ const SimpleCard = (props) => {
 
   return (
     <div className={classes.SimpleCard}>
+      {(props.modifyMode) ? <SubmitBox language={props.language} masterKey={props.masterKey} label={getSelectKey(props.allData, 'label', props.language, 'fr')} /> : null}
       {logo}
       {title}
       {label}
@@ -36,8 +41,12 @@ const SimpleCard = (props) => {
 export default SimpleCard;
 
 SimpleCard.propTypes = {
+  language: PropTypes.string.isRequired,
   logo: PropTypes.string,
   title: PropTypes.string,
   label: PropTypes.string,
   tooltip: PropTypes.string,
+  masterKey: PropTypes.string, // Utilisée pour le mode modifier/enrichir
+  modifyMode: PropTypes.bool,
+  allData: PropTypes.object.isRequired,
 };
