7eea390ff38f2243f0a2bcf859b59c83214cd720
folland <frederic.olland@gmail.com>
2019-10-11 16:24:10 +0200

    persons

--- src/Components/Results/Person/Components/PersonNameCard.js
+++ src/Components/Results/Person/Components/PersonNameCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 
-import classes from './ParticipantRow.scss';
+import classes from './PersonNameCard.scss';
 import getSelectKey from '../../../../Utils/getSelectKey';
 
 /* Gestion des langues */
@@ -21,51 +21,20 @@ const messages = {
  * Accessible : .
  * Tests unitaires : .
 */
-const ParticipantRow = props => (
+const PersonNameCard = props => (
   <IntlProvider locale={props.language} messages={messages[props.language]}>
-    <div className={classes.participant}>
-      {
-        (props.data.role === 'coordinator')
-          ? (
-            <div className="d-flex justify-content-start align-items-bottom pr-2">
-              <span className={classes.yellowBullet} />
-              <FormattedHTMLMessage id="Project.participantRow.coordinator" defaultMessage="Project.participantRow.coordinator" />
-            </div>
-          )
-          : null
-
-      }
-      <div className={classes.Title}>
-        {
-          (props.data.structure)
-            ? getSelectKey(props.data.structure, 'label', props.language, 'default')
-            : getSelectKey(props.data, 'label', props.language, 'default')
-        }
-      </div>
-      <div className={`d-flex justify-content-between align-items-bottom pr-2 ${classes.Funding}`}>
-        {
-          (props.data.funding)
-            ? (
-              <div>{`${props.data.funding} €`}</div>
-            )
-            : null
-        }
-        {
-          (props.data.structure && props.data.structure.address.length > 0 && props.data.structure.address[0].country)
-            ? (
-              <div>{`${props.data.structure.address[0].country}`}</div>
-            )
-            : null
-        }
-      </div>
+    <div>
+      {props.data.fullName}
+      {props.data.gender}
+      {JSON.stringify(props.data.externalIds)}
     </div>
   </IntlProvider>
 );
 
 
-export default ParticipantRow;
+export default PersonNameCard;
 
-ParticipantRow.propTypes = {
+PersonNameCard.propTypes = {
   language: PropTypes.string.isRequired,
   data: PropTypes.object,
 };
