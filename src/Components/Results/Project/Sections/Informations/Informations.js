f71ed06cd36f5e857c3a754dcef21d0c776e9352
folland <frederic.olland@gmail.com>
2019-10-18 18:37:00 +0200

    end Projects persons

--- src/Components/Results/Project/Sections/Informations/Informations.js
+++ src/Components/Results/Project/Sections/Informations/Informations.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
-import moment from 'moment';
 
 import SectionTitle from '../../../../Shared/Results/SectionTitle/SectionTitle';
-import SimpleCard from '../../../../Shared/Ui/SimpleCard/SimpleCard2';
-import getSelectKey from '../../../../../Utils/getSelectKey';
+import Identity from './SubSections/Identity';
+import Status from './SubSections/Status';
 
 import classes from './Informations.scss';
 
@@ -30,118 +29,38 @@ const Informations = (props) => {
   const sectionStyle = {
     backgroundImage: `url(${bgUrl})`,
   };
-  const filterKeys = ['label', 'startDate', 'id', 'endDate', 'acronym', 'type'];
-  let filtered = null;
-  if (Object.keys(props.data).length > 0) {
-    filtered = Object.keys(props.data)
-      .filter(key => filterKeys.includes(key))
-      .reduce((obj, key) => ({
-        ...obj,
-        [key]: props.data[key],
-      }));
-  }
-  if (filtered) {
-    const startDate = moment(filtered.startDate).format('L');
-    const endDate = moment(filtered.endDate).format('L');
-    const title = getSelectKey(filtered, 'label', props.language, 'fr');
-    const acro = ` (${getSelectKey(filtered, 'acronym', 'default', 'fr')})`;
-    const titleAcro = title + acro;
-    return (
-      <section className={`container-fluid ${classes.Informations}`} style={sectionStyle}>
-        <IntlProvider locale={props.language} messages={messages[props.language]}>
-          <div className="container">
-            <SectionTitle
-              icon="fas fa-id-card"
-              modifyModeHandle={props.modifyModeHandle}
-              modifyModeKey="informations"
+  return (
+    <section className={`container-fluid ${classes.Informations}`} style={sectionStyle}>
+      <IntlProvider locale={props.language} messages={messages[props.language]}>
+        <div className="container">
+          <SectionTitle
+            icon="fas fa-id-card"
+            modifyModeHandle={props.modifyModeHandle}
+            modifyModeKey="informations"
+            modifyMode={props.modifyMode}
+          >
+            <FormattedHTMLMessage id="Project.informations.title" defaultMessage="Project.informations.title" />
+          </SectionTitle>
+          <div className="d-flex flex-wrap">
+            <Identity
+              language={props.language}
+              data={props.data}
+              masterKey="Project/informations"
               modifyMode={props.modifyMode}
-            >
-              <FormattedHTMLMessage id="Project.informations.title" defaultMessage="Project.informations.title" />
-            </SectionTitle>
-            <div className="row">
-              <div className={`d-flex flex-wrap align-self-start ${classes.noSpace50}`}>
-                <div className={classes.noSpace100}>
-                  <SimpleCard
-                    language={props.language}
-                    logo="fas fa-calendar-day"
-                    title={messages[props.language]['Project.informations.name']}
-                    label={titleAcro}
-                    tooltip=""
-                    masterKey="Project/title"
-                    modifyMode={props.modifyMode}
-                    allData={props.data}
-                  />
-                </div>
-                {
-                  (startDate)
-                    ? (
-                      <div className={classes.noSpace50}>
-                        <SimpleCard
-                          language={props.language}
-                          logo="fas fa-id-card"
-                          title={messages[props.language]['Project.informations.startDate']}
-                          label={startDate}
-                          tooltip=""
-                          masterKey="Project/startDate"
-                          modifyMode={props.modifyMode}
-                          allData={props.data}
-                        />
-                      </div>
-                    )
-                    : null
-                }
-                {
-                  (endDate)
-                    ? (
-                      <div className={classes.noSpace50}>
-                        <SimpleCard
-                          language={props.language}
-                          logo="fas fa-id-card"
-                          title={messages[props.language]['Project.informations.endDate']}
-                          label={endDate}
-                          tooltip=""
-                          masterKey="Project/endDate"
-                          modifyMode={props.modifyMode}
-                          allData={props.data}
-                        />
-                      </div>
-                    )
-                    : null
-                }
-              </div>
-              <div className={`${classes.noSpace50} d-flex flex-wrap align-self-start`}>
-                <div className={classes.noSpace50}>
-                  <SimpleCard
-                    language={props.language}
-                    logo="fas fa-calendar-day"
-                    title={messages[props.language]['Project.informations.type']}
-                    label={props.data.type}
-                    tooltip=""
-                    masterKey="Project/type"
-                    modifyMode={props.modifyMode}
-                    allData={props.data}
-                  />
-                </div>
-                <div className={classes.noSpace50}>
-                  <SimpleCard
-                    language={props.language}
-                    logo="fas fa-qrcode"
-                    title={messages[props.language]['Project.informations.identifier']}
-                    label={props.data.id}
-                    tooltip=""
-                    masterKey="Project/identifier"
-                    modifyMode={props.modifyMode}
-                    allData={props.data}
-                  />
-                </div>
-              </div>
-            </div>
+              allData={props.data}
+            />
+            <Status
+              language={props.language}
+              data={props.data}
+              masterKey="Project/informations"
+              modifyMode={props.modifyMode}
+              allData={props.data}
+            />
           </div>
-        </IntlProvider>
-      </section>
-    );
-  }
-  return null;
+        </div>
+      </IntlProvider>
+    </section>
+  );
 };
 
 export default Informations;
---
+++ src/Components/Results/Project/Sections/Informations/SubSections/Status.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
+import moment from 'moment';
+import 'moment/locale/fr';
+
+
+import SimpleCard from '../../../../../Shared/Ui/SimpleCard/SimpleCard2';
+import PileCard from '../../../Components/PileCard';
+
+import classes from './SubSectionsStyles.scss';
+
+import messagesFr from '../../../translations/fr.json';
+import messagesEn from '../../../translations/en.json';
+
+const messages = {
+  fr: messagesFr,
+  en: messagesEn,
+};
+/**
+ * Informations
+ * Url : .
+ * Informations : .
+ * Responsive : .
+ * Accessible : .
+ * Tests unitaires : .
+*/
+
+const Status = (props) => {
+  moment.locale(props.language);
+  let calculatedEndDate;
+  if (props.data.startDate && props.data.duration) {
+    const dt = new Date(props.data.startDate);
+    dt.setMonth(dt.getMonth() + props.data.duration);
+    calculatedEndDate = dt;
+  }
+  const statusEndDate = (props.data.endDate) ? new Date(props.data.endDate) : calculatedEndDate;
+  const status = (statusEndDate > Date.now()) ? 'running' : 'over';
+  const startDate = moment(props.data.startDate).format('LL');
+  const endDate = (props.data.endDate) ? moment(props.data.endDate).format('LL') : moment(calculatedEndDate).format('LL');
+  let percents = 100;
+  if (status && status === 'running') {
+    percents = Math.round(
+      ((Date.now() - new Date(props.data.startDate)) * 100 / (new Date(statusEndDate) - new Date(props.data.startDate))),
+    );
+  }
+  return (
+    <IntlProvider locale={props.language} messages={messages[props.language]}>
+      <section className={`p-0 ${classes.W50}`}>
+        <div className={classes.SubSectionTitle}>
+          <FormattedHTMLMessage
+            id="Project.informations.status.title"
+            defaultMessage="Project.informations.status.title"
+          >
+            {
+              txt => (
+                <h3 className={classes.Title}>
+                  {txt}
+                </h3>
+              )
+            }
+          </FormattedHTMLMessage>
+        </div>
+        <div className="d-flex flex-wrap">
+          <div className={classes.W50}>
+            {
+              (startDate)
+                ? (
+                  <SimpleCard
+                    language={props.language}
+                    logo="fas fa-qrcode"
+                    title={messages[props.language]['Project.informations.startDate']}
+                    label={startDate}
+                    tooltip=""
+                    masterKey="Project/startDate"
+                    modifyMode={props.modifyMode}
+                    allData={props.data}
+                  />
+                )
+                : null
+            }
+          </div>
+          <div className={classes.W50}>
+            {
+              (endDate)
+                ? (
+                  <SimpleCard
+                    language={props.language}
+                    logo="fas fa-qrcode"
+                    title={messages[props.language]['Project.informations.endDate']}
+                    label={endDate}
+                    tooltip=""
+                    masterKey="Project/endDate"
+                    modifyMode={props.modifyMode}
+                    allData={props.data}
+                  />
+                )
+                : null
+            }
+          </div>
+          <div className={classes.W50}>
+            {
+              (percents)
+                ? (
+                  <PileCard
+                    language={props.language}
+                    percents={percents}
+                  />
+                )
+                : null
+            }
+          </div>
+        </div>
+      </section>
+    </IntlProvider>
+  );
+};
+
+export default Status;
+
+Status.propTypes = {
+  language: PropTypes.string.isRequired,
+  data: PropTypes.object,
+  modifyMode: PropTypes.bool.isRequired,
+};
