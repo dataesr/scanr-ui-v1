ab8a84a533fa6cab2b35a80bc0271154058655e9
jeremy.peglion <jeremy.peglion@gmail.com>
2019-10-22 16:43:54 +0200

    update

---
+++ src/Components/Shared/svg/logo-scanr-v2.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SVG = props => (
+  <svg
+    width={props.width}
+    height="110"
+    xmlns="http://www.w3.org/2000/svg"
+    className={`svg-icon ${props.className || ''}`}
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+  >
+    <g id="Symboles">
+      <polygon id="Trait" style={{ fill: '#FFFFFF' }} points="331.2,85 45.5,85 45.5,91 331.2,91" />
+      <g id="Logo">
+        <g>
+          <circle style={{ fill: '#1F3657' }} cx="45.5" cy="45.5" r="42.5" />
+          <path
+            style={{ fill: '#FFFFFF' }}
+            d="M45.5,6C67.3,6,85,23.7,85,45.5S67.3,85,45.5,85S6,67.3,6,45.5S23.7,6,45.5,6 M45.5,0 C20.4,0,0,20.4,0,45.5S20.4,91,45.5,91S91,70.6,91,45.5S70.6,0,45.5,0L45.5,0z"
+          />
+        </g>
+        <path style={{ fill: '#FFFFFF' }} d="M46.4,45.6L24,26.8L23,28l22.4,18.7L46.4,45.6z" />
+        <path style={{ fill: '#FFFFFF' }} d="M51.9,44L53,27.1L51.5,27l-1.1,16.9L51.9,44z" />
+        <path style={{ fill: '#FFFFFF' }} d="M56.5,47.9l16.8-8.6L72.7,38l-16.8,8.6L56.5,47.9z" />
+        <path style={{ fill: '#FFFFFF' }} d="M54.2,55.2l8.6,9.8l1.1-1l-8.6-9.8L54.2,55.2z" />
+        <path style={{ fill: '#FFFFFF' }} d="M45.2,53L29.7,63.7l0.8,1.2L46,54.2L45.2,53z" />
+        <path
+          style={{
+            fill: 'none', stroke: '#FFD138', 'stroke-width': 3, 'stroke-miterlimit': 10,
+          }}
+          d="M45.9,47c-1.1,1.7-1.2,3.9-0.3,5.7l0.8,1.1 c1.9,2.1,5.1,2.5,7.4,0.9l1-0.9c1.4-1.6,1.8-3.8,1.1-5.8l-0.6-1.2c-0.8-1.2-2.1-2.1-3.6-2.4l-1.4-0.1c-1.3,0.1-2.6,0.6-3.6,1.6
+          L45.9,47z"
+        />
+        <path
+          style={{
+            fill: '#FFFFFF', stroke: '#5DD99D', 'stroke-width': 2, 'stroke-miterlimit': 10,
+          }}
+          d="M53,29.9c2.7-0.3,4.7-2.7,4.4-5.4 c-0.3-2.7-2.7-4.7-5.4-4.4c-2.7,0.3-4.7,2.7-4.4,5.4c0.2,2.1,1.8,3.9,3.9,4.3L53,29.9z"
+        />
+        <path
+          style={{
+            fill: '#FFFFFF', stroke: '#CC3D8F', 'stroke-width': 1.5, 'stroke-miterlimit': 10,
+          }}
+          d="M69.4,41.2c1.1,1.3,3.1,1.4,4.4,0.3 c1.3-1.1,1.4-3.1,0.3-4.4c-0.6-0.7-1.5-1.1-2.4-1.1l0,0c-1.7,0-3.1,1.4-3.1,3.2c0,0.2,0,0.5,0.1,0.7L69.4,41.2z"
+        />
+        <path
+          style={{
+            fill: '#FFFFFF', stroke: '#FE655B', 'stroke-width': 1.5, 'stroke-miterlimit': 10,
+          }}
+          d="M60.2,62.2c-1.3,1.7-0.9,4.1,0.7,5.4 c1.7,1.3,4.1,0.9,5.4-0.7c1.3-1.7,0.9-4.1-0.7-5.4c-1.2-0.9-2.9-1-4.2-0.3L60.2,62.2z"
+        />
+        <path
+          style={{
+            fill: '#FFFFFF', stroke: '#FEAD40', 'stroke-width': 2, 'stroke-miterlimit': 10,
+          }}
+          d="M33.4,62c-1.9-2.1-5.2-2.2-7.3-0.3 s-2.2,5.2-0.3,7.3c1.9,2.1,5.2,2.2,7.3,0.3c1.7-1.5,2.1-4,1.1-6L33.4,62z"
+        />
+        <path
+          style={{
+            fill: '#FFFFFF', stroke: '#FE7747', 'stroke-width': 1.5, 'stroke-miterlimit': 10,
+          }}
+          d="M26.9,29.7c0.3-0.5,0.5-1.2,0.5-1.8 c0-2.1-1.7-3.8-3.8-3.8s-3.8,1.7-3.8,3.8s1.7,3.8,3.8,3.8c0.9,0,1.7-0.3,2.4-0.8L26.9,29.7z"
+        />
+      </g>
+    </g>
+    <g id="Texte">
+      <path
+        id="s_1_"
+        style={{
+          fill: 'none', stroke: '#FFFFFF', 'stroke-width': 4, 'stroke-miterlimit': 10,
+        }}
+        d="M136.9,49.8c0-0.4,0-0.7-0.1-1.1
+        c-0.6-3.2-3.8-5.7-7.7-5.7H108c-4.3,0-7.8,3-7.8,6.7v2.4c0,3.7,3.5,6.7,7.8,6.7h10.5h3.4h7.3c4.3,0,7.8,3,7.8,6.7v2.4
+        c0,3.7-3.5,6.7-7.8,6.7H108c-3.9,0-7.2-2.5-7.7-5.8c0-0.3-0.1-0.6-0.1-0.9"
+      />
+      <path
+        id="C_1_"
+        style={{
+          fill: 'none', stroke: '#FFFFFF', 'stroke-width': 4, 'stroke-miterlimit': 10,
+        }}
+        d="M184.7,68.2c0,3.7-3.5,6.7-7.8,6.7
+        h-21.1c-4.3,0-7.8-3-7.8-6.7v-2.4V52.3v-2.4c0-3.7,3.5-6.7,7.8-6.7h21.1c4.3,0,7.8,3,7.8,6.7"
+      />
+      <path
+        id="a_1_"
+        style={{
+          fill: 'none', stroke: '#FFFFFF', 'stroke-width': 4, 'stroke-miterlimit': 10,
+        }}
+        d="M195.8,49.8c0-3.7,3.5-6.7,7.8-6.7h21.1
+        c4.3,0,7.8,3,7.8,6.7v18.4c0,3.7-3.5,6.7-7.8,6.7h-21.1c-4.3,0-7.8-3-7.8-6.7v-2.4c0-3.7,3.5-6.7,7.8-6.7h28.9"
+      />
+      <path
+        id="n_1_"
+        style={{
+          fill: 'none', stroke: '#FFFFFF', 'stroke-width': 4, 'stroke-miterlimit': 10,
+        }}
+        d="M243.7,76.9V49.8c0-3.7,3.5-6.7,7.8-6.7
+        h2.8h15.5h2.8c4.3,0,7.8,3,7.8,6.7v27.1"
+      />
+      <path
+        id="R_1_"
+        style={{
+          fill: 'none', stroke: '#FFFFFF', 'stroke-width': 6, 'stroke-miterlimit': 10,
+        }}
+        d="M291.5,76.9V33.9c0-3.7,3.5-6.7,7.8-6.7
+        h21.1c4.3,0,7.8,3,7.8,6.7v8.4c0,3.7-3.5,7.4-7.8,7.4h-10.6l18.3,27.1"
+      />
+      <text
+        transform="matrix(1.05 0 0 1 97.3237 107.0652)"
+        style={{
+          fill: '#FFFFFF', 'font-family': 'ArialMT', 'font-size': '12px',
+        }}
+      >
+        Moteur de la Recherche et de l’Innovation
+      </text>
+    </g>
+  </svg>
+);
+
+export default SVG;
+
+SVG.propTypes = {
+  width: PropTypes.string,
+  className: PropTypes.string,
+};
+
+SVG.defaultProps = {
+  width: '100%',
+  className: '',
+};
