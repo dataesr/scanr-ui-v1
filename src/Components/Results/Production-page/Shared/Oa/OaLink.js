6818369bef2c0137c6b0392b645443eb18781cca
folland <frederic.olland@gmail.com>
2019-10-22 18:51:38 +0200

    add changes

--- src/Components/Results/Production-page/Shared/Oa/OaLink.js
+++ src/Components/Results/Production-page/Shared/Oa/OaLink.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import getSelectKey from '../../../../../Utils/getSelectKey';
-import SubmitBox from '../../../../Shared/SubmitBox/SubmitBox';
+// import getSelectKey from '../../../../../Utils/getSelectKey';
+// import SubmitBox from '../../../../Shared/SubmitBox/SubmitBox';
 
 /* Gestion des langues */
 import messagesFr from './translations/fr.json';
