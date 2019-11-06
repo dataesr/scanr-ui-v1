177b078bf691c0df686b041552b3b1868e19157a
Jerem <jeremy.peglion@gmail.com>
2019-11-06 21:17:10 +0100

    update + fix

--- src/Components/Results/Entity-page/Team/SubComponents/nbPersonsCard.js
+++ src/Components/Results/Entity-page/Team/SubComponents/nbPersonsCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ButtonWithModal from '../../../../Shared/Ui/Buttons/ButtonWithModal';
 import classes from './nbPersonsCard.scss';
 
 import logo from '../../../../Shared/svg/icon-fiche-responsable_h.svg';
+
+/* Gestion des langues */
+import messagesFr from '../translations/fr.json';
+import messagesEn from '../translations/en.json';
+
+const messages = {
+  fr: messagesFr,
+  en: messagesEn,
+};
 
 /**
  * nbPersonsCard
@@ -14,7 +24,7 @@ import logo from '../../../../Shared/svg/icon-fiche-responsable_h.svg';
  * Tests unitaires : .
 */
 const nbPersonsCard = props => (
-  <div className={classes.nbPersonsCard}>
+  <div className={`d-flex flex-column ${classes.nbPersonsCard}`}>
     <div className={classes.Logo}>
       <img src={logo} alt="logo" />
       <img src={logo} alt="logo" />
@@ -26,12 +36,21 @@ const nbPersonsCard = props => (
     <div className={classes.Label}>
       personnes identifiées
     </div>
+    <div className="mt-auto" style={{ marginBottom: '8px' }}>
+      <ButtonWithModal
+        logo="fas fa-expand"
+        title={messages[props.language].allTeamLabel}
+        buttonLabel={messages[props.language].showAll}
+        dataHtml={props.personsModalList}
+      />
+    </div>
   </div>
 );
 
 export default nbPersonsCard;
 
 nbPersonsCard.propTypes = {
-  // language: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
   nbPersons: PropTypes.number.isRequired,
+  personsModalList: PropTypes.any,
 };
