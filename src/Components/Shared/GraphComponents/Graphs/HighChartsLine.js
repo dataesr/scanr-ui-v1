8ac2bb14911793bf72e48029547c78f3250d1668
eric <eric.jeangirard@recherche.gouv.fr>
2019-11-08 16:43:17 +0100

    remove useless

--- src/Components/Shared/GraphComponents/Graphs/HighChartsLine.js
+++ src/Components/Shared/GraphComponents/Graphs/HighChartsLine.js
@@ -51,7 +51,7 @@ export default class HighChartsLine extends Component {
         enabled: false,
       },
       tooltip: {
-          enabled: false
+        enabled: false,
       },
       title: {
         text: '',
@@ -82,29 +82,28 @@ export default class HighChartsLine extends Component {
         // reversed: true
       },
       plotOptions: {
-	      fillColor: {
-                        linearGradient: {
-                            x1: 0,
-                            y1: 0,
-                            x2: 0,
-                            y2: 1
-                        },
-                        stops: [
-                            [0, Highcharts.getOptions().colors[0]],
-                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                        ]
-                    },
-                    marker: {
-                        radius: 2
-                    },
-                    lineWidth: 1,
-                    states: {
-                        hover: {
-                            lineWidth: 1
-                        }
-                    },
-                    threshold: null,
-      
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1,
+          },
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')],
+          ],
+        },
+        marker: {
+          radius: 2,
+        },
+        lineWidth: 1,
+        states: {
+          hover: {
+            lineWidth: 1,
+          },
+        },
+        threshold: null,
         series: {
           stacking: 'normal',
           pointPadding: 0,
