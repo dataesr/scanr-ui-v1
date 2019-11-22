0884cc046a33ace00889eabbd0c0699e9dd817b8
eric <eric.jeangirard@recherche.gouv.fr>
2019-11-22 13:59:42 +0100

    focus

--- src/Components/Shared/GraphComponents/Graphs/HighChartsPackedbubble.js
+++ src/Components/Shared/GraphComponents/Graphs/HighChartsPackedbubble.js
@@ -1,55 +1,187 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+import HCAccessibility from 'highcharts/modules/accessibility';
+import HCExporting from 'highcharts/modules/exporting';
+import HCExportingData from 'highcharts/modules/export-data';
+import HCRounded from 'highcharts-rounded-corners';
 import HCmore from 'highcharts/highcharts-more.src';
-import PropTypes from 'prop-types';
+import classes from '../GraphComponents.scss';
 
 HCmore(Highcharts);
 
 
-const PackedBubbleChart = (props) => {
-  const options = {
-    chart: {
-      type: 'packedbubble',
-    },
-    colors: ['#43ab92', '#f75f00', '#c93838', '#512c62', '#8f4426', '#64ccda', '#5f6769', '#ff78ae', '#00818a', '#0c093c'],
-    exporting: { enabled: false },
-    credits: { enabled: false },
-    title: {
-      text: props.text,
-    },
-    tooltip: {
-      useHTML: true,
-      pointFormat: '<b>{point.name}:</b> {point.value} '.concat(props.tooltipText),
-    },
-    series: props.data,
-    plotOptions: {
-      packedbubble: {
-        useSimulation: true,
-        minSize: '10%',
-        maxSize: '100%',
-        layoutAlgorithm: {
-          splitSeries: (props.data.length > 2),
-          seriesInteraction: false,
-          dragBetweenSeries: false,
-          parentNodeLimit: true,
+HCAccessibility(Highcharts);
+HCExporting(Highcharts);
+HCExportingData(Highcharts);
+HCRounded(Highcharts);
+
+/**
+ * HighChartsPackedbubble
+ * Url : <br/>
+ * Description : Composant HighCharts qui rend les barres horizontales <br/>
+ * Responsive : . <br/>
+ * Accessible : . <br/>
+ * Tests unitaires : . <br/>.
+*/
+
+export default class HighChartsPackedbubble extends Component {
+  constructor(props) {
+    super(props);
+    this.chart = React.createRef();
+    this.data = this.props.data;
+    this.tooltipText = this.props.tooltipText;
+    this.state = {
+      options: null,
+    };
+    this.exportChartPdf = this.exportChartPdf.bind(this);
+    this.exportChartPng = this.exportChartPng.bind(this);
+    this.exportChartCsv = this.exportChartCsv.bind(this);
+  }
+
+  componentDidMount() {
+    const options = {
+      credits: {
+        enabled: false,
+      },
+      chart: {
+        type: 'packedbubble',
+      },
+      tooltip: {
+        useHTML: true,
+        pointFormat: '<b>{point.name}:</b> {point.value} '.concat(this.tooltipText),
+      },
+      colors: ['#43ab92', '#f75f00', '#c93838', '#512c62', '#8f4426', '#64ccda', '#5f6769', '#ff78ae', '#00818a', '#0c093c'],
+      title: {
+        text: (this.props.text) ? (this.props.text) : '',
+      },
+      series: this.data,
+      plotOptions: {
+        packedbubble: {
+          useSimulation: true,
+          minSize: '10%',
+          maxSize: '100%',
+          layoutAlgorithm: {
+            splitSeries: (this.data.length > 2),
+            seriesInteraction: false,
+            dragBetweenSeries: false,
+            parentNodeLimit: true,
+          },
         },
       },
-    },
-  };
+      exporting: {
+        filename: this.props.filename,
+        buttons: {
+          contextButton: {
+            enabled: false,
+          },
+        },
+        chartOptions: {
+          title: {
+            text: this.props.filename,
+          },
+          credits: {
+            enabled: true,
+            text: "Source : ScanR, Moteur de la Recherche et de l'Innovation",
+          },
+        },
+      },
+    };
+    this.setState({ options });
+  }
 
-  return (
-    <HighchartsReact
-      highcharts={Highcharts}
-      options={options}
-    />
-  );
+  exportChartPdf() {
+    this.chart.current.chart.exportChart({
+      type: 'application/pdf',
+    });
+  }
+
+  exportChartPng() {
+    this.chart.current.chart.exportChart({
+      type: 'image/png',
+    });
+  }
+
+  exportChartCsv() {
+    this.chart.current.chart.downloadCSV();
+  }
+
+  render() {
+    const ShareComponent = () => (
+      <div className={`d-flex flex-wrap pl-4 pr-4 p-3 ${classes.ShareComponent}`}>
+        <div className="mr-auto d-flex align-items-center">
+          <div className="pr-1 d-flex align-items-center">
+            <span className={`pr-2 ${classes.ShareTexts}`}>Partager</span>
+            <button type="button" className={classes.Button}>
+              <i className="fas fa-share-alt-square" />
+            </button>
+          </div>
+          <div className="pr-1 d-flex align-items-center">
+            <span className={`pr-2 pl-3 ${classes.ShareTexts}`}>Intégrer le code</span>
+            <button type="button" className={classes.Button}>
+              <i className="fas fa-code" />
+            </button>
+          </div>
+        </div>
+        <div className="d-flex align-items-center">
+          <div className={`pr-2 ${classes.ShareTexts}`}>Télécharger:</div>
+          <div className="pr-1 d-flex align-items-center">
+            <button type="button" onClick={this.exportChartPdf} className={classes.Button}>
+              <i className="fas fa-file-pdf" />
+            </button>
+            <span className={`pr-1 ${classes.ShareTexts}`}>.pdf</span>
+          </div>
+          <div className="pr-1 d-flex align-items-center">
+            <button type="button" onClick={this.exportChartPng} className={classes.Button}>
+              <i className="fas fa-image" />
+            </button>
+            <span className={`pr-1 ${classes.ShareTexts}`}>.png</span>
+          </div>
+          <div className="pr-1 d-flex align-items-center">
+            <button type="button" onClick={this.exportChartCsv} className={classes.Button}>
+              <i className="fas fa-table" />
+            </button>
+            <span className={classes.ShareTexts}>.csv</span>
+          </div>
+        </div>
+      </div>
+    );
+    return (
+      <div>
+        {
+          this.state.options !== null
+            ? (
+              <div>
+                <hr className={classes.HorizontalBar} />
+                <div className="pl-4">
+                  <HighchartsReact
+                    highcharts={Highcharts}
+                    options={this.state.options}
+                    ref={this.chart}
+                  />
+                </div>
+                <hr className={classes.HorizontalBar} />
+                { this.props.exporting ? (
+                  <ShareComponent />) : null }
+              </div>
+            )
+            : <div>Loading...</div>
+        }
+      </div>
+    );
+  }
+}
+
+HighChartsPackedbubble.defaultProps = {
+  data: { entries: [] },
+  exporting: false,
 };
 
-export default PackedBubbleChart;
-
-PackedBubbleChart.propTypes = {
-  data: PropTypes.array.isRequired,
+HighChartsPackedbubble.propTypes = {
+  filename: PropTypes.string.isRequired,
+  data: PropTypes.object,
   text: PropTypes.string,
+  exporting: PropTypes.bool,
   tooltipText: PropTypes.string,
 };
