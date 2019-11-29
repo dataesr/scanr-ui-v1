4e9fb115ac934bb278fb6e21b919651d588ec048
jeremy.peglion <jeremy.peglion@gmail.com>
2019-11-29 12:02:12 +0100

    translation

--- src/Components/Shared/GraphComponents/Graphs/HighChartsWordCloud.js
+++ src/Components/Shared/GraphComponents/Graphs/HighChartsWordCloud.js
@@ -7,7 +7,8 @@ import HCExporting from 'highcharts/modules/exporting';
 import HCExportingData from 'highcharts/modules/export-data';
 import HCRounded from 'highcharts-rounded-corners';
 import wordCloudModule from 'highcharts/modules/wordcloud';
-import ReactPiwik from 'react-piwik';
+import ShareComponent from './SubComponents/ShareComponent';
+
 import classes from '../GraphComponents.scss';
 
 wordCloudModule(Highcharts);
@@ -36,9 +37,6 @@ export default class HighChartsWordCloud extends Component {
     this.state = {
       options: null,
     };
-    this.exportChartPdf = this.exportChartPdf.bind(this);
-    this.exportChartPng = this.exportChartPng.bind(this);
-    this.exportChartCsv = this.exportChartCsv.bind(this);
   }
 
   componentDidMount() {
@@ -105,59 +103,7 @@ export default class HighChartsWordCloud extends Component {
     this.setState({ options });
   }
 
-  exportChartPdf() {
-    ReactPiwik.push(['trackEvent', 'Download', 'PDF_'.concat(this.props.filename)]);
-    this.chart.current.chart.exportChart({
-      type: 'application/pdf',
-    });
-  }
-
-  exportChartPng() {
-    ReactPiwik.push(['trackEvent', 'Download', 'PNG_'.concat(this.props.filename)]);
-    this.chart.current.chart.exportChart({
-      type: 'image/png',
-    });
-  }
-
-  exportChartCsv() {
-    ReactPiwik.push(['trackEvent', 'Download', 'CSV_'.concat(this.props.filename)]);
-    this.chart.current.chart.downloadCSV();
-  }
-
   render() {
-    const ShareComponent = () => (
-      <div className={`d-flex flex-wrap pl-4 pr-4 p-3 ${classes.ShareComponent}`}>
-        <div className="mr-auto d-flex align-items-center">
-          <div className="pr-1 d-flex align-items-center">
-            <span className={`pr-2 ${classes.ShareTexts}`}>Partager</span>
-            <button type="button" className={classes.Button}>
-              <i className="fas fa-share-alt-square" />
-            </button>
-          </div>
-        </div>
-        <div className="d-flex align-items-center">
-          <div className={`pr-2 ${classes.ShareTexts}`}>Télécharger:</div>
-          <div className="pr-1 d-flex align-items-center">
-            <button type="button" onClick={this.exportChartPdf} className={classes.Button}>
-              <i className="fas fa-file-pdf" />
-            </button>
-            <span className={`pr-1 ${classes.ShareTexts}`}>.pdf</span>
-          </div>
-          <div className="pr-1 d-flex align-items-center">
-            <button type="button" onClick={this.exportChartPng} className={classes.Button}>
-              <i className="fas fa-image" />
-            </button>
-            <span className={`pr-1 ${classes.ShareTexts}`}>.png</span>
-          </div>
-          <div className="pr-1 d-flex align-items-center">
-            <button type="button" onClick={this.exportChartCsv} className={classes.Button}>
-              <i className="fas fa-table" />
-            </button>
-            <span className={classes.ShareTexts}>.csv</span>
-          </div>
-        </div>
-      </div>
-    );
     return (
       <div>
         {
@@ -173,7 +119,11 @@ export default class HighChartsWordCloud extends Component {
                   />
                 </div>
                 <hr className={classes.HorizontalBar} />
-                <ShareComponent />
+                <ShareComponent
+                  language={this.props.language}
+                  filename={this.props.filename}
+                  chart={this.chart}
+                />
               </div>
             )
             : <div>Loading...</div>
@@ -188,6 +138,7 @@ HighChartsWordCloud.defaultProps = {
 };
 
 HighChartsWordCloud.propTypes = {
+  language: PropTypes.string.isRequired,
   filename: PropTypes.string.isRequired,
   data: PropTypes.object,
 };
