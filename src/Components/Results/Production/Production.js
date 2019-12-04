485b7632745229be8480cf501cf614b1e99bccb1
folland <frederic.olland@gmail.com>
2019-12-04 19:30:14 +0100

    start patents

--- src/Components/Results/Production/Production.js
+++ src/Components/Results/Production/Production.js
@@ -1,15 +1,16 @@
-import React, { Component, Fragment } from 'react';
-import Axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
+import useGetData from '../../../Hooks/useGetData';
+import Loader from '../../Shared/LoadingSpinners/RouterSpinner';
+import Errors from '../../Shared/Errors/Errors';
 
 import { API_PUBLICATIONS_END_POINT } from '../../../config/config';
-// import getSelectKey from '../../../Utils/getSelectKey';
 
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 
-
 import Publication from './Publication/Publication';
+import Patent from './Patent/Patent';
 import Thesis from './Thesis/Thesis';
 
 /**
@@ -20,84 +21,42 @@ import Thesis from './Thesis/Thesis';
  * Accessible : .
  * Tests unitaires : .
 */
-class Production extends Component {
-  state = {
-    data: {},
+const Production = (props) => {
+  const renderContent = (prop) => {
+    const properties = {
+      language: prop.language,
+      data: prop.data,
+      id: prop.match.params.id,
+    };
+    switch (prop.data.productionType) {
+      case 'patent': return <Patent {...properties} />;
+      case 'thesis': return <Thesis {...properties} />;
+      case 'publication': return <Publication {...properties} />;
+      // For now, default is patent. Raise errors after data are fixed ?
+      default: return <Patent {...properties} />;
+    }
   };
 
-  componentDidMount() {
-    const { id } = this.props.match.params;
-    this.getData(id);
+  const { data, isLoading, isError } = useGetData(API_PUBLICATIONS_END_POINT, props.match.params.id);
+
+  if (isLoading) {
+    return <Loader />;
   }
-
-  getData(id) {
-    // Récupéraion des données de l'entité
-    const url = `${API_PUBLICATIONS_END_POINT}/${id}`;
-    Axios.get(url)
-      .then((response) => {
-        this.setState({ data: response.data });
-        /* eslint-disable-next-line */
-      }).catch(e => console.log('erreur=>', e));
+  if (isError) {
+    return <Errors />;
   }
-
-  renderPublication = () => (
-    <Fragment>
-      <Publication
-        language={this.props.language}
-        data={this.state.data}
-        id={this.props.match.params.id}
-      />
-    </Fragment>
-  )
-
-  renderThesis = () => (
-    <Fragment>
-      <Thesis
-        language={this.props.language}
-        data={this.state.data}
-        id={this.props.match.params.id}
-      />
-    </Fragment>
-  )
-
-  renderPattent = () => (
-    <Fragment>
-      Brevet
-    </Fragment>
-  )
-
-  render() {
-    if (!this.state.data) {
-      return <Fragment>Chargement ...</Fragment>;
-    }
-    let content = null;
-    if (this.state.data.productionType === 'publication') {
-      content = this.renderPublication();
-    } else if (this.state.data.productionType === 'thesis' && this.state.data.id.indexOf('these') !== -1) {
-      content = this.renderThesis();
-    } else if (this.state.data.productionType === 'thesis' && this.state.data.id.indexOf('these') === -1) {
-      content = this.renderPublication();
-    } else if (this.state.data.productionType === 'pattent') {
-      content = this.renderPattent();
-    }
-
-    return (
-      <Fragment>
-        <Header
-          language={this.props.language}
-          switchLanguage={this.props.switchLanguage}
-        />
-        {content}
-        <Footer language={this.props.language} />
-      </Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Header />
+      {renderContent({ ...props, data })}
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default Production;
 
 Production.propTypes = {
-  language: PropTypes.string.isRequired,
+  // language: PropTypes.string.isRequired,
   match: PropTypes.object.isRequired,
-  switchLanguage: PropTypes.func.isRequired,
 };
