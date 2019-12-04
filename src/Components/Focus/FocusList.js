f046bb691a652ee3e4bbd588f0149c5818610c15
folland <frederic.olland@gmail.com>
2019-12-04 13:15:40 +0100

    focus from API

--- src/Components/Focus/FocusList.js
+++ src/Components/Focus/FocusList.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Composants
+import Loader from '../Shared/LoadingSpinners/RouterSpinner';
 import Banner from '../Shared/Banner/Banner';
 import Footer from '../Shared/Footer/Footer';
 import Header from '../Shared/Header/Header';
 import HeaderTitle from '../Shared/HeaderTitle/HeaderTitle';
-import LastFocus from '../Shared/LastFocus/LastFocus';
+import FocusCard from '../Shared/Ui/FocusCard/FocusCard';
+import useGetData from '../../Hooks/useGetData';
+
 // import Lexicon from '../Shared/Lexicon/Lexicon';
 // import Search from '../Home-page/Search/Search';
-
-import classes from '../Home/Home.scss';
-// import classes from './FocusList.scss';
+import classes from './Focus.scss';
 
 /**
  * FocusList
@@ -22,29 +23,62 @@ import classes from '../Home/Home.scss';
  * Tests
  */
 
-const FocusList = props => (
-  <div className={`container-fluid ${classes.HomePage}`}>
-    <Header
-      language={props.language}
-      switchLanguage={props.switchLanguage}
-    />
-    <HeaderTitle
-      language={props.language}
-      labelkey="focus"
-      url1="/"
-      url2="/focus"
-    />
-    <LastFocus language={props.language} />
-    <Banner
-      language={props.language}
-      labelKey="DiscoverDataesr"
-      cssClass="BannerDark"
-      url={`https://data.esr.gouv.fr/${props.language.toUpperCase()}/`}
-      target="_blank"
-    />
-    <Footer language={props.language} />
-  </div>
-);
+const FocusList = (props) => {
+  const { data, isLoading, isError } = useGetData('http://66.70.222.205/api/focus');
+  if (isError) {
+    return <div>ERROR...</div>;
+  }
+  if (isLoading) {
+    return <Loader />;
+  }
+  if (!data.data) {
+    return <div>NO DATA</div>;
+  }
+  return (
+    <React.Fragment>
+      <Header
+        language={props.language}
+        switchLanguage={props.switchLanguage}
+      />
+      <HeaderTitle
+        language={props.language}
+        labelkey="focus"
+        url1="/"
+        url2="/focus"
+      />
+      <section className={classes.FocusSection}>
+        <div className="container">
+          <div className="row py-3">
+            {
+              data.data.map(oneFocus => (
+                <div className={`col-lg-4 ${classes.CardContainer}`} key={oneFocus.title.fr}>
+                  {/* eslint-disable */}
+                  {/* disableReason: Does not accept _id */}
+                  <FocusCard
+                    schema={oneFocus.api}
+                    tags={oneFocus.tags[props.language]}
+                    title={oneFocus.title[props.language]}
+                    type="bubble"
+                    url={`/focus/${oneFocus._id}`}
+                  />
+                {/* eslint-enable */}
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+      <Banner
+        language={props.language}
+        labelKey="DiscoverDataesr"
+        cssClass="BannerDark"
+        url="https://data.esr.gouv.fr/"
+        target="_blank"
+      />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default FocusList;
 
