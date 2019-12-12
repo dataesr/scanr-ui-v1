52981a52c06c828d528c456c9a8ac4eead98d617
folland <frederic.olland@gmail.com>
2019-12-12 16:12:14 +0100

    fix thesis and search

--- src/Components/Results/Person/Components/ThesisParticipationsCard.js
+++ src/Components/Results/Person/Components/ThesisParticipationsCard.js
@@ -1,19 +1,11 @@
 import React, { Fragment } from 'react';
 // import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
 import ReactTooltip from 'react-tooltip';
-import { IntlProvider } from 'react-intl';
 
 import PropTypes from 'prop-types';
 
 import classes from './PersonNameCard.scss';
 import ButtonWithModal from '../../../Shared/Ui/Buttons/ButtonWithModal';
-import messagesFr from '../translations/fr.json';
-import messagesEn from '../translations/en.json';
-
-const messages = {
-  fr: messagesFr,
-  en: messagesEn,
-};
 
 /**
  * ThesisParticipations component
@@ -34,25 +26,23 @@ const ThesisParticipations = (props) => {
   if (props.dataHtml.length > 0) {
     return (
       <Fragment>
-        <IntlProvider locale={props.language} messages={messages[props.language]}>
-          <div className={`d-flex flex-column pb-3 ${classes.ParticipationsCard}`}>
-            <div className="mt-auto pb-2">
-              <p className={`m-0 ${classes.Number}`}>
-                {props.dataHtml.length}
-              </p>
-              <p className={`m-0 ${classes.Title}`}>
-                {props.title}
-              </p>
-            </div>
-            <ButtonWithModal
-              logo="fas fa-qrcode"
-              title={props.title}
-              buttonLabel={props.buttonLabel}
-              dataHtml={props.dataHtml}
-            />
-            {tooltip}
+        <div className={`d-flex flex-column pb-3 ${classes.ParticipationsCard}`}>
+          <div className="mt-auto pb-2">
+            <p className={`m-0 ${classes.Number}`}>
+              {props.dataHtml.length}
+            </p>
+            <p className={`m-0 ${classes.Title}`}>
+              {props.title}
+            </p>
           </div>
-        </IntlProvider>
+          <ButtonWithModal
+            logo="fas fa-qrcode"
+            title={props.title}
+            buttonLabel={props.buttonLabel}
+            dataHtml={props.dataHtml}
+          />
+          {tooltip}
+        </div>
       </Fragment>
     );
   }
@@ -62,7 +52,6 @@ const ThesisParticipations = (props) => {
 export default ThesisParticipations;
 
 ThesisParticipations.propTypes = {
-  language: PropTypes.string,
   dataHtml: PropTypes.any,
   title: PropTypes.object,
   buttonLabel: PropTypes.object,
