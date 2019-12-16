4fa72a152a0064f7ed85d1cf0968347d8101c55b
eric <eric.jeangirard@recherche.gouv.fr>
2019-12-16 19:56:28 +0100

    welcome modal

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
+import Modal from 'react-bootstrap/Modal';
 import { GlobalContext } from '../../GlobalContext';
 import useScrollY from '../../Hooks/useScrollY';
 
@@ -25,54 +26,75 @@ const msg = {
 };
 
 const HomePage = (props) => {
+  const [isActive, setActive] = useState(true);
   const context = useContext(GlobalContext);
   const scrollY = useScrollY();
-
+  let displayWelcomeModal = true;
+  const visited = localStorage.alreadyVisitedScanR;
+  if (visited) {
+    displayWelcomeModal = false;
+  } else {
+    localStorage.alreadyVisitedScanR = true;
+  }
   return (
     <IntlProvider locale={context.language} messages={msg[context.language]}>
-      <div className={`container-fluid ${classes.HomePage}`}>
-        <FormattedHTMLMessage id="Home.title">
-          {logoLabel => (<ScanRMeta title={logoLabel} />)}
-        </FormattedHTMLMessage>
-        <Header />
-        <Search
-          {...props}
-          language={context.language}
-          isFull={scrollY === 0}
-        />
-        <ScanrToday
-          language={context.language}
-          isFull={scrollY === 0}
-        />
-        <Banner
-          language={context.language}
-          labelKey="WhatAreOurSources"
-          cssClass="BannerLight"
-          url="/ressources"
-        />
+      <Fragment>
+        <Modal
+          show={displayWelcomeModal && isActive}
+          onHide={() => setActive(!isActive)}
+          size="lg"
+        >
+          <Modal.Header closeButton className={classes.Header}>
+            <p className={classes.Title}>
+              <i className="fas fa-bookmark" />
+              <FormattedHTMLMessage id="Welcome.Title" />
+            </p>
+          </Modal.Header>
+          <Modal.Body>
+            <div className={`d-flex flex-column align-items-center p-4 ${classes.Modal}`}>
+              <i className="fas fa-lg-edit" />
+              <FormattedHTMLMessage id="Welcome.Text" />
+            </div>
+          </Modal.Body>
+        </Modal>
+        <div className={`container-fluid ${classes.HomePage}`}>
+          <FormattedHTMLMessage id="Home.title">
+            {logoLabel => (<ScanRMeta title={logoLabel} />)}
+          </FormattedHTMLMessage>
+          <Header />
+          <Search
+            {...props}
+            language={context.language}
+            isFull={scrollY === 0}
+          />
+          <ScanrToday
+            language={context.language}
+            isFull={scrollY === 0}
+          />
+          <Banner
+            language={context.language}
+            labelKey="WhatAreOurSources"
+            cssClass="BannerLight"
+            url="/ressources"
+          />
 
-        <MostActiveThemes
-          language={context.language}
-          data={currentThemes}
-        />
-        {/* <Banner
-          language={context.language}
-          labelKey="Appear"
-          cssClass="BannerLight"
-          url=""
-        /> */}
-        <LastFocus language={context.language} />
-        {/* Not for Now */}
-        {/* <Newsletter language={this.context.language} /> */}
-        <Banner
-          language={context.language}
-          labelKey="DiscoverDataesr"
-          cssClass="BannerDark"
-          url="https://data.esr.gouv.fr/"
-          target="_blank"
-        />
-        <Footer />
-      </div>
+          <MostActiveThemes
+            language={context.language}
+            data={currentThemes}
+          />
+          <LastFocus language={context.language} />
+          {/* Not for Now */}
+          {/* <Newsletter language={this.context.language} /> */}
+          <Banner
+            language={context.language}
+            labelKey="DiscoverDataesr"
+            cssClass="BannerDark"
+            url="https://data.esr.gouv.fr/"
+            target="_blank"
+          />
+          <Footer />
+        </div>
+      </Fragment>
     </IntlProvider>
   );
 };
