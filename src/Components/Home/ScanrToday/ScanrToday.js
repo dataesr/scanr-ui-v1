a36001c5376b421edb74dd1a3221cf9d8343aa96
folland <frederic.olland@gmail.com>
2019-12-19 16:37:24 +0100

    scanrToday

--- src/Components/Home/ScanrToday/ScanrToday.js
+++ src/Components/Home/ScanrToday/ScanrToday.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormattedHTMLMessage, FormattedNumber } from 'react-intl';
+// import { FormattedHTMLMessage, FormattedNumber } from 'react-intl';
 // import useSearchAPI from '../../../Hooks/useSearchAPI';
 // import {
 //   API_STRUCTURES_SEARCH_END_POINT,
@@ -7,16 +7,19 @@ import { FormattedHTMLMessage, FormattedNumber } from 'react-intl';
 //   API_PROJECTS_SEARCH_END_POINT,
 //   API_PUBLICATIONS_SEARCH_END_POINT,
 // } from '../../../config/config';
-
 // import LexiconModal from '../../Shared/Lexicon/LexiconModal/LexiconModal';
 import entityLogo from '../../../images/svg/icon-entities.svg';
 import personLogo from '../../../images/svg/icon-persons.svg';
 import projectLogo from '../../../images/svg/icon-projects.svg';
 import productionLogo from '../../../images/svg/icon-publications.svg';
-import scanRLogo from '../../Shared/svg/logo-scanr-white.svg';
-
-import { DatabaseText } from './styles';
-import classes from './ScanrToday.scss';
+import scanRLogo from '../../Shared/svg/logo-scanr-white_V3.svg';
+import {
+  DatabaseText,
+  Chevron,
+  ChevronContainer,
+  ChevronStart,
+  ChevronEnd,
+} from './styles';
 
 
 const ScanrToday = () => {
@@ -36,71 +39,64 @@ const ScanrToday = () => {
   // const persons = useSearchAPI(API_PERSONS_SEARCH_END_POINT, request);
   // const projects = useSearchAPI(API_PROJECTS_SEARCH_END_POINT, request);
   // const productions = useSearchAPI(API_PUBLICATIONS_SEARCH_END_POINT, request);
-
   // Use entities.data, persons.data etc. for search counts.
-
   return (
-    <section className={classes.ScanrToday}>
-      <div className={`d-flex flex-wrap justify-content-between ${classes.database}`}>
-        <div className={`p-2 ${classes.chevronStart}`}>
-          <span>
-            <img
-              src={scanRLogo}
-              alt="Logo Entity scanR"
-              aria-hidden
-            />
-            <DatabaseText><FormattedHTMLMessage id="Home.ScanrToday.scanrData" /></DatabaseText>
-          </span>
-        </div>
-        <div className={`col-auto  ${classes.chevron}`}>
-          <span>
-            <a href="/recherche/structures">
-              <img
-                src={entityLogo}
-                alt="Logo Entity scanR"
-                aria-hidden
-              />
-              <DatabaseText><FormattedHTMLMessage id="Home.ScanrToday.cardEntity" /></DatabaseText>
-            </a>
-          </span>
-        </div>
-        <div className={`col-auto ${classes.chevron}`}>
-          <span>
-            <a href="/recherche/persons">
-              <img
-                src={personLogo}
-                alt="Logo person scanR"
-                aria-hidden
-              />
-              <DatabaseText><FormattedHTMLMessage id="Home.ScanrToday.cardAuthor" /></DatabaseText>
-            </a>
-          </span>
-        </div>
-        <div className={`col-auto ${classes.chevron}`}>
-          <span>
-            <a href="/recherche/projects">
-              <img
-                src={projectLogo}
-                alt="Logo projets scanR"
-                aria-hidden
-              />
-              <DatabaseText><FormattedHTMLMessage id="Home.ScanrToday.cardProject" /></DatabaseText>
-            </a>
-          </span>
-        </div>
-        <div className={`col-auto ${classes.chevronEnd}`}>
-          <span>
-            <a href="/recherche/publications">
-              <img
-                src={productionLogo}
-                alt="Logo productions scanR"
-                aria-hidden
-              />
-              <DatabaseText><FormattedHTMLMessage id="Home.ScanrToday.cardProduction" /></DatabaseText>
-            </a>
-          </span>
-        </div>
-      </div>
+    <section>
+      <ChevronContainer>
+        <ChevronStart>
+          <img
+            src={scanRLogo}
+            alt="Logo Entity scanR"
+            aria-hidden
+            style={{ maxHeight: '70px' }}
+          />
+          <DatabaseText>35&nbsp;000 entités publiques et privées</DatabaseText>
+        </ChevronStart>
+        <Chevron>
+          <img
+            src={entityLogo}
+            alt=""
+            aria-hidden
+            style={{ maxHeight: '70px' }}
+          />
+          <DatabaseText>
+            35&nbsp;000 entités publiques et privées
+          </DatabaseText>
+        </Chevron>
+        <Chevron>
+          <img
+            src={personLogo}
+            alt=""
+            aria-hidden
+            style={{ maxHeight: '70px' }}
+          />
+          <DatabaseText>
+            35&nbsp;000 entités publiques et privées
+          </DatabaseText>
+        </Chevron>
+        <Chevron>
+          <img
+            src={projectLogo}
+            alt=""
+            aria-hidden
+            style={{ maxHeight: '70px' }}
+          />
+          <DatabaseText>
+            35&nbsp;000 entités publiques et privées
+          </DatabaseText>
+        </Chevron>
+        <ChevronEnd>
+          <img
+            src={productionLogo}
+            alt=""
+            aria-hidden
+            style={{ maxHeight: '70px' }}
+          />
+          <DatabaseText>
+            35&nbsp;000 entités publiques et privées
+          </DatabaseText>
+        </ChevronEnd>
+      </ChevronContainer>
     </section>
   );
 };
