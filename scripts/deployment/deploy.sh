2f73a8ec8814e41a9954d3a55d425c957e101b7e
eric <eric.jeangirard@recherche.gouv.fr>
2019-12-31 16:19:36 +0100

    env variable

--- scripts/deployment/deploy.sh
+++ scripts/deployment/deploy.sh
@@ -1,9 +1,10 @@
 ssh -l ejeangirard@default@pr-mgl-web01.magical.in.phm.education.gouv.fr:ejeangirard ssh.in.phm.education.gouv.fr << EOF
-mkdir -p scanr && cd scanr && rm -rf scanr-v2 && git clone https://dataesr:dataESR2019@github.com/jerem1508/scanr-v2.git && cd scanr-v2 && npm install && npm run build
+#mkdir -p scanr && cd scanr && rm -rf scanr-v2 && git clone https://dataesr:dataESR2019@github.com/jerem1508/scanr-v2.git && cd scanr-v2 && npm install && npm run build
 #cd scanr && cd scanr-v2
+cd scanr/scanr-v2 && git fetch --all && git reset --hard origin/master && npm install && npm run build
+/bin/cp -rf build/ /www/. && cd /www
 #sudo -E -iu root << IN_EOF
 #/bin/cp -rf /export/home/ejeangirard/scanr/scanr-v2/build/ /www/. && cd /www && chmod -R 755 build
-/bin/cp -rf build/ /www/. && cd /www
 #IN_EOF
 EOF
 
