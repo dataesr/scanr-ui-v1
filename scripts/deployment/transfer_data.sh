84e1c4ad910b97e6e4557d182edf446eae6eafbd
eric <eric.jeangirard@recherche.gouv.fr>
2020-01-06 13:09:32 +0100

    script to export data to sword

---
+++ scripts/deployment/transfer_data.sh
@@ -0,0 +1,8 @@
+lftp sftp://menesr:9747TK78aU@51.91.212.5:139 << EOF
+cd upload
+put scanrJSON/organizations.json
+put scanrJSON/projects.json
+put scanrJSON/persons.json
+put scanrJSON/publications.json
+bye
+EOF
