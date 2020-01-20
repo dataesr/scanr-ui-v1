b9bf4ba2a3087dfdad5b96d7bb9ca5738ad1154a
eric <eric.jeangirard@recherche.gouv.fr>
2020-01-20 09:21:24 +0100

    translation share in header

--- src/Components/Shared/ShareModal/ShareModal.js
+++ src/Components/Shared/ShareModal/ShareModal.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
 import queryString from 'query-string';
 import Modal from 'react-bootstrap/Modal';
+import { GlobalContext } from '../../../GlobalContext';
 
 import classes from './ShareModal.scss';
+
+/* Gestion des langues */
+import messagesFr from '../Header/translations/fr.json';
+import messagesEn from '../Header/translations/en.json';
 
 const basesUrls = ['linkedin', 'twitter', 'facebook'];
 basesUrls.linkedin = 'https://www.linkedin.com/sharing/share-offsite/?url=';
@@ -30,39 +36,53 @@ const share = (socialMedia) => {
 
 const ShareModal = () => {
   const [showModal, setShowModal] = useState(false);
+  const context = useContext(GlobalContext);
+
+  const messages = {
+    fr: messagesFr,
+    en: messagesEn,
+  };
 
   return (
-    <React.Fragment>
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <h1 className={classes.Title}>
-            <i className="fas fa-share-alt" />
-            &nbsp;
-            Partager
-          </h1>
-        </Modal.Header>
+    <IntlProvider locale={context.language} messages={messages[context.language]}>
+      <React.Fragment>
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <h1 className={classes.Title}>
+              <i className="fas fa-share-alt" />
+              &nbsp;
+              <FormattedHTMLMessage
+                id="Header.share"
+                defaultMessage="Header.share"
+              />
+            </h1>
+          </Modal.Header>
 
-        <Modal.Body>
-          <div className={`d-flex justify-content-around ${classes.Socials}`}>
-            <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('linkedin'); setShowModal(false); }}>
-              <i className="fab fa-linkedin" title="" />
-            </button>
-            <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('twitter'); setShowModal(false); }}>
-              <i className="fab fa-twitter-square" title="" />
-            </button>
-            <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('facebook'); setShowModal(false); }}>
-              <i className="fab fa-facebook-square" title="" />
-            </button>
-          </div>
-        </Modal.Body>
-      </Modal>
+          <Modal.Body>
+            <div className={`d-flex justify-content-around ${classes.Socials}`}>
+              <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('linkedin'); setShowModal(false); }}>
+                <i className="fab fa-linkedin" title="" />
+              </button>
+              <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('twitter'); setShowModal(false); }}>
+                <i className="fab fa-twitter-square" title="" />
+              </button>
+              <button type="button" className={classes.ButtonNoStyled} onClick={() => { share('facebook'); setShowModal(false); }}>
+                <i className="fab fa-facebook-square" title="" />
+              </button>
+            </div>
+          </Modal.Body>
+        </Modal>
 
-      <span className={classes.Link} onClick={() => setShowModal(true)} onKeyPress={() => setShowModal(true)} role="button" tabIndex={0}>
-        <i className="fas fa-share-alt" />
-        &nbsp;
-        Partager
-      </span>
-    </React.Fragment>
+        <span className={classes.Link} onClick={() => setShowModal(true)} onKeyPress={() => setShowModal(true)} role="button" tabIndex={0}>
+          <i className="fas fa-share-alt" />
+          &nbsp;
+          <FormattedHTMLMessage
+            id="Header.share"
+            defaultMessage="Header.share"
+          />
+        </span>
+      </React.Fragment>
+    </IntlProvider>
   );
 };
 
