d9f0223e5a3791405569677587d6fb1c08a96504
eric <eric.jeangirard@recherche.gouv.fr>
2020-02-06 15:57:16 +0100

    export scripts

--- scripts/deployment/transfer_data_preprod.sh
+++ scripts/deployment/transfer_data_preprod.sh
@@ -1,7 +1,7 @@
 # a faire une fois 
 # ssh -p 22 menesrscanesr@sftplyon.sword-group.com
 lftp sftp://menesrscanesr:eNigA2K4P5q7@sftplyon.sword-group.com << EOF
-cd Data
+cd Data/Input
 put scanrJSON/organizations.json
 put scanrJSON/projects.json
 put scanrJSON/persons.json
