80d9205a64c44658b50ada0b4702cdcaae05e3c3
folland87 <frederic@olland.me>
2021-01-07 17:17:46 +0100

    add crossrefParser

--- src/Components/Results/Production/Publication/Sections/Citations/Citations.js
+++ src/Components/Results/Production/Publication/Sections/Citations/Citations.js
@@ -21,20 +21,44 @@ import classes from './Citations.scss';
 */
 const messages = { fr: messagesFr, en: messagesEn };
 
-const CrossRefList = ({ ids, language }) => {
+function parseCrossRef(items) {
+  return items.map((item) => {
+    const obj = {};
+    obj.doi = item.DOI;
+    if (item.title.length) {
+      obj.title = item.title[0];
+    }
+    if (item.issued && item.issued['date-parts'].length && item.issued['date-parts'][0].length) {
+      obj.date = item.issued['date-parts'][0][0];
+    }
+    if (item['container-title'] && item['container-title'].length) {
+      obj.journal = item['container-title'][0];
+    }
+    if (item.author && item.author.length) {
+      obj.num_authors = item.author.length;
+      obj.authors = item.author.map(
+        auth => `${(auth.given) ? `${auth.given} ` : ''}${(auth.family) ? auth.family : ''}`,
+      );
+    }
+    return obj;
+  });
+}
+
+function CrossRefList({ ids, language }) {
   const { data, isLoading, isError } = useCrossRef(ids);
   if (isLoading) return <SectionLoader />;
   if (isError) return <Errors error={500} />;
   if (data.length) {
+    const publis = parseCrossRef(data);
     return (
       <div className="row">
         {
-          data.slice(0, 6).map(item => (
+          publis.slice(0, 6).map(item => (
             <IntlProvider locale={language} messages={messages[language]}>
               <div key={item.DOI} className={`col-md-4 ${classes.CardContainer}`}>
                 <article className={`d-flex flex-column ${classes.ResultCard} ${classes.CardWhite}`}>
                   <h3 className={`mb-auto pb-2 ${classes.CardTitle}`}>
-                    {(item.title && item.title.length) && item.title[0]}
+                    {item.title || <FormattedHTMLMessage id="Publication.unknown" />}
                   </h3>
                   <ul className="m-0 p-0">
                     <li className="d-flex">
@@ -43,16 +67,15 @@ const CrossRefList = ({ ids, language }) => {
                       </div>
                       <p className="m-0">
                         {
-                          (item.author && item.author.length < 3)
-                            ? item.author.map(auth => `${auth.given} ${auth.family}`)
+                          (item.num_authors && item.num_authors < 3)
+                            ? item.authors.join(', ')
                             : (
                               <Fragment>
-                                {`${item.author[0].given} ${item.author[0].family} `}
-                                <FormattedHTMLMessage id="Publication.authors" values={{ count: item.author.length }} />
+                                {`${item.authors[0]} `}
+                                <FormattedHTMLMessage id="Publication.authors" values={{ count: item.num_authors }} />
                               </Fragment>
                             )
                         }
-
                       </p>
                     </li>
                     <li className="d-flex">
@@ -60,7 +83,7 @@ const CrossRefList = ({ ids, language }) => {
                         <i aria-hidden="true" className="fas fa-calendar" />
                       </div>
                       <p className="m-0">
-                        {item.issued && item.issued['date-parts'][0][0]}
+                        {item.date || <FormattedHTMLMessage id="Publication.unknown" />}
                       </p>
                     </li>
                     <li className="d-flex">
@@ -68,7 +91,7 @@ const CrossRefList = ({ ids, language }) => {
                         <i aria-hidden="true" className="fas fa-folder-open" />
                       </div>
                       <p className="m-0">
-                        {item['container-title'][0]}
+                        {item.journal || <FormattedHTMLMessage id="Publication.unknown" />}
                       </p>
                     </li>
                   </ul>
@@ -84,7 +107,7 @@ const CrossRefList = ({ ids, language }) => {
     );
   }
   return null;
-};
+}
 
 CrossRefList.propTypes = {
   ids: PropTypes.array.isRequired,
