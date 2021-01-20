86ea211aefc376b4c9c7aedd036ed5a1ab2739f9
folland87 <frederic@olland.me>
2021-01-20 12:06:24 +0100

    delete action => new repo created dataesr/kubectl-deploy

--- actions/README.md
+++
@@ -1,3 +0,0 @@
-From https://github.com/myrotvorets/kubectl-action
-
-Make our own to have control over entrypoint.sh.
--- actions/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo "$KUBE_CONFIG" | base64 -d > /tmp/kubeconfig
-export KUBECONFIG=/tmp/kubeconfig
-
-exec /usr/bin/kubectl rollout restart deployment/$target
