9923aaea5043c6a80ccc249bac76b47f62293374
paulinegc <paulinegc@zoho.com>
2021-01-21 13:36:53 +0100

    review Intl and close modal inside component

--- src/Components/Shared/Ui/Modal/UIModal.js
+++ src/Components/Shared/Ui/Modal/UIModal.js
@@ -1,49 +1,74 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 
+/* Intl */
+import { IntlProvider, FormattedHTMLMessage } from 'react-intl';
+import { GlobalContext } from '../../../../GlobalContext';
+import messagesFr from './translations/fr.json';
+import messagesEn from './translations/en.json';
+
 /* SCSS */
 import './UIModal.static.scss';
+import classes from '../../../Search/Filters/ActiveFilterCard/ActiveFilterCard.scss';
+
+const msg = {
+  fr: messagesFr,
+  en: messagesEn,
+};
 
 function UIModal(props) {
   const {
-    title, children, toggleModal, active,
+    title, children, active,
   } = props;
+  const [opened, setOpened] = useState(active);
+  const context = useContext(GlobalContext);
+
   useEffect(() => {
     ReactModal.setAppElement('body');
-  });
-  return (
-    <ReactModal
-      closeTimeoutMS={200}
-      contentLabel={title}
-      onRequestClose={toggleModal}
-      isOpen={active}
-      className="Modal"
+    setOpened(active);
+  }, [active]);
 
-    >
-      <section className="modal-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-9">
-              <article>
-                <div className="wrapper-title">
-                  {title ? <h1 className="title pl-4">{title}</h1> : null}
-                </div>
-              </article>
-            </div>
-            <div className="col-3">
-              <button type="button" className="modal-close close pointer" onClick={toggleModal}>
-                <span aria-hidden="true">×</span>
-                <span className="sr-only">Close</span>
-              </button>
+  const closeModal = () => {
+    setOpened(false);
+  };
+
+  return (
+    <IntlProvider locale={context.language} messages={msg[context.language]}>
+      <ReactModal
+        closeTimeoutMS={200}
+        contentLabel={title}
+        onRequestClose={closeModal}
+        isOpen={opened}
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        <section className="modal-header">
+          <div className="container">
+            <div className="row">
+              <div className="col-9">
+                <article>
+                  <div className="wrapper-title">
+                    {title ? <h1 className="title pl-4">{title}</h1> : null}
+                  </div>
+                </article>
+              </div>
+              <div className="col-3">
+                <button type="button" className="modal-close close pointer" onClick={closeModal}>
+                  <span aria-hidden="true">
+                    <i className={`fas fa-xs fa-times ${classes.closeIcon}`} />
+                  </span>
+                  <span className="sr-only"><FormattedHTMLMessage id="close" /></span>
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section>
-        {children}
-      </section>
-    </ReactModal>
+        </section>
+        <section>
+          {children}
+        </section>
+      </ReactModal>
+    </IntlProvider>
   );
 }
 
@@ -52,7 +77,6 @@ export default UIModal;
 UIModal.propTypes = {
   active: PropTypes.bool,
   title: PropTypes.string,
-  toggleModal: PropTypes.func,
   children: PropTypes.any.isRequired,
 };
 
