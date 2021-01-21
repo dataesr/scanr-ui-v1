22d590bfc1691445dd3a3f3b698fbcf56aa9741d
paulinegc <paulinegc@zoho.com>
2021-01-21 09:39:14 +0100

    review

--- src/Components/Shared/Ui/Modal/UIModal.js
+++ src/Components/Shared/Ui/Modal/UIModal.js
@@ -1,15 +1,11 @@
-import React, { useContext, useEffect } from 'react';
-import { IntlProvider } from 'react-intl';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
-
-import { GlobalContext } from '../../../../GlobalContext';
 
 /* SCSS */
 import './UIModal.static.scss';
 
 function UIModal(props) {
-  const context = useContext(GlobalContext);
   const {
     title, children, toggleModal, active,
   } = props;
@@ -17,39 +13,37 @@ function UIModal(props) {
     ReactModal.setAppElement('body');
   });
   return (
-    <IntlProvider locale={context.language}>
-      <ReactModal
-        closeTimeoutMS={200}
-        contentLabel={title}
-        onRequestClose={toggleModal}
-        isOpen={active}
-        className="Modal"
-        overlayClassName="Overlay"
-      >
-        <section className="modal-header">
-          <div className="container">
-            <div className="row">
-              <div className="col-9">
-                <article>
-                  <div className="wrapper-title">
-                    {title ? <h4 className="title pl-4">{title}</h4> : null}
-                  </div>
-                </article>
-              </div>
-              <div className="col-3">
-                <button type="button" className="modal-close close pointer" onClick={toggleModal}>
-                  <span aria-hidden="true">×</span>
-                  <span className="sr-only">Close</span>
-                </button>
-              </div>
+    <ReactModal
+      closeTimeoutMS={200}
+      contentLabel={title}
+      onRequestClose={toggleModal}
+      isOpen={active}
+      className="Modal"
+
+    >
+      <section className="modal-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-9">
+              <article>
+                <div className="wrapper-title">
+                  {title ? <h1 className="title pl-4">{title}</h1> : null}
+                </div>
+              </article>
+            </div>
+            <div className="col-3">
+              <button type="button" className="modal-close close pointer" onClick={toggleModal}>
+                <span aria-hidden="true">×</span>
+                <span className="sr-only">Close</span>
+              </button>
             </div>
           </div>
-        </section>
-        <section>
-          {children}
-        </section>
-      </ReactModal>
-    </IntlProvider>
+        </div>
+      </section>
+      <section>
+        {children}
+      </section>
+    </ReactModal>
   );
 }
 
