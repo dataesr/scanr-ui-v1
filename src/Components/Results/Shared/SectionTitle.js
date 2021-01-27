aa9b33add236c321eb473a465c7518d812ecda85
paulinegc <francasix@outlook.com>
2021-01-27 19:45:13 +0100

    add font-size to icon

--- src/Components/Results/Shared/SectionTitle.js
+++ src/Components/Results/Shared/SectionTitle.js
@@ -13,7 +13,7 @@ import classes from './SectionTitle.scss';
  * Responsive : .
  * Accessible : .
  * Tests unitaires : .
-*/
+ */
 const SectionTitle = props => (
   <div className="row">
     <div className="col-lg-12">
@@ -21,21 +21,21 @@ const SectionTitle = props => (
         <i className={`fas ${props.icon} ${classes.Icon}`} />
         <span className={`pl-0 pl-lg-2 my-2 ${classes.Title}`}>
           {(props.total) ? (props.total).toLocaleString(props.language) : null}
-        &nbsp;
+          &nbsp;
           {props.title}
-        &nbsp;
+          &nbsp;
           {(props.lexicon && !props.subTitle) ? (
             <LexiconModal language={props.language} target={props.lexicon}>
-              <i className="fa fa-info-circle" />
+              <i className={`${classes.Title} fa fa-info-circle pointer`} />
             </LexiconModal>
-          ) : null }
+          ) : null}
         </span>
         {props.subTitle ? (
           <React.Fragment>
             <div className="ml-xl-3 mb-sm-3 mb-xl-0 d-flex align-items-center">
               <div className="d-flex">
                 <LexiconModal language={props.language} target={props.lexicon}>
-                  <i className={`fa fa-info-circle ${classes.Icon}`} />
+                  <i className={`fa fa-info-circle pointer ${classes.Icon}`} />
                 </LexiconModal>
               </div>
               <div
@@ -54,64 +54,68 @@ const SectionTitle = props => (
         )
           : null}
         {
-        (props.viewModeClickHandler && props.viewMode)
-          ? (
-            <div className="d-flex flex-wrap align-items-center">
-              <div
-                role="button"
-                tabIndex={0}
-                aria-labelledby="ViewList"
-                onClick={() => props.viewModeClickHandler('list')}
-                onKeyPress={() => props.viewModeClickHandler('list')}
-                className={classes.ViewChangeButton}
-              >
-                <div className="d-flex flex-nowrap align-items-center">
-                  <span className={`mr-2 btn ${classes.SquareButton} ${(props.viewMode === 'list') ? classes.btn_scanrBlue : classes.btn_scanrlightgrey}`}>
-                    <i aria-hidden className="fas fa-list" />
-                  </span>
-                  <p className={`m-0 ${classes.Text}`} id="ViewList">
-                    {
-                      (props.language === 'fr')
-                        ? 'Liste'
-                        : 'List'
-                    }
-                  </p>
+          (props.viewModeClickHandler && props.viewMode)
+            ? (
+              <div className="d-flex flex-wrap align-items-center">
+                <div
+                  role="button"
+                  tabIndex={0}
+                  aria-labelledby="ViewList"
+                  onClick={() => props.viewModeClickHandler('list')}
+                  onKeyPress={() => props.viewModeClickHandler('list')}
+                  className={classes.ViewChangeButton}
+                >
+                  <div className="d-flex flex-nowrap align-items-center">
+                    <span
+                      className={`mr-2 btn ${classes.SquareButton} ${(props.viewMode === 'list') ? classes.btn_scanrBlue : classes.btn_scanrlightgrey}`}
+                    >
+                      <i aria-hidden className="fas fa-list" />
+                    </span>
+                    <p className={`m-0 ${classes.Text}`} id="ViewList">
+                      {
+                        (props.language === 'fr')
+                          ? 'Liste'
+                          : 'List'
+                      }
+                    </p>
+                  </div>
+                </div>
+                <div
+                  role="button"
+                  tabIndex={0}
+                  aria-labelledby="ViewGraph"
+                  onClick={() => props.viewModeClickHandler('graph')}
+                  onKeyPress={() => props.viewModeClickHandler('graph')}
+                >
+                  <div className="mx-3 d-flex flex-nowrap align-items-center">
+                    <span
+                      className={`mx-2 btn ${classes.SquareButton} ${(props.viewMode === 'graph') ? classes.btn_scanrBlue : classes.btn_scanrlightgrey}`}
+                    >
+                      <i aria-hidden className="fas fa-chart-pie" />
+                    </span>
+                    <p className={`m-0 ${classes.Text}`} id="ViewGraph">
+                      Visualisation
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div
-                role="button"
-                tabIndex={0}
-                aria-labelledby="ViewGraph"
-                onClick={() => props.viewModeClickHandler('graph')}
-                onKeyPress={() => props.viewModeClickHandler('graph')}
-              >
-                <div className="mx-3 d-flex flex-nowrap align-items-center">
-                  <span className={`mx-2 btn ${classes.SquareButton} ${(props.viewMode === 'graph') ? classes.btn_scanrBlue : classes.btn_scanrlightgrey}`}>
-                    <i aria-hidden className="fas fa-chart-pie" />
-                  </span>
-                  <p className={`m-0 ${classes.Text}`} id="ViewGraph">
-                    Visualisation
-                  </p>
-                </div>
-              </div>
-            </div>
-          )
-          : null
-      }
+            )
+            : null
+        }
         {
-        (props.id && props.objectType && !props.subTitle)
-          ? (
-            <span className="pl-2 ml-auto my-2">
-              <Contribute
-                language={props.language}
-                sectionName={props.title}
-                objectId={props.id}
-                objectType={props.objectType}
-              />
-            </span>
-          )
-          : null
-      }
+          (props.id && props.objectType && !props.subTitle)
+            ? (
+              <span className="pl-2 ml-auto my-2">
+                <Contribute
+                  language={props.language}
+                  sectionName={props.title}
+                  objectId={props.id}
+                  objectType={props.objectType}
+                />
+              </span>
+            )
+            : null
+        }
       </div>
     </div>
   </div>
