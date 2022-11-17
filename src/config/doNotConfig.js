7d6a20e7e3f7d9fdfaa8ff5bf6bc01156bd6806a
eric <eric.jeangirard@recherche.gouv.fr>
2022-11-17 09:26:48 +0100

    remove a publi

--- src/config/doNotConfig.js
+++ src/config/doNotConfig.js
@@ -3,10 +3,10 @@ export const doNotSearch = {
     ids: ['idref227790677'],
   },
   publications: {
-    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128'],
+    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
   },
 };
 export const doNotDisplay = {
-  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128'],
+  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
   person: ['idref227790677'],
 };
