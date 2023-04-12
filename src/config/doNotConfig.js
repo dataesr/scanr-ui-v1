549e77d736a2ae4afc4cc1aad70747cb4f55e429
folland87 <frederic@olland.me>
2023-04-12 15:00:24 +0200

    delete a publication

--- src/config/doNotConfig.js
+++ src/config/doNotConfig.js
@@ -3,10 +3,10 @@ export const doNotSearch = {
     ids: ['idref227790677'],
   },
   publications: {
-    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
+    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209', 'doi10.1037/aca0000422'],
   },
 };
 export const doNotDisplay = {
-  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
+  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209', 'doi10.1037%252faca0000422'],
   person: ['idref227790677'],
 };
