c0e8d5e4db6699024aa401cc1246ce2c7d55672b
eric <eric.jeangirard@recherche.gouv.fr>
2023-07-21 13:17:23 +0200

    extra doNotConfig

--- src/config/doNotConfig.js
+++ src/config/doNotConfig.js
@@ -1,12 +1,12 @@
 export const doNotSearch = {
   persons: {
-    ids: ['idref227790677'],
+    ids: ['idref227790677', 'idref257915222'],
   },
   publications: {
-    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
+    ids: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209', 'nnt2021coaz0011'],
   },
 };
 export const doNotDisplay = {
-  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209'],
-  person: ['idref227790677'],
+  publication: ['hal-02423632', 'hal-02422378', 'hal-02415294', 'hal-02415565', 'dumas-02948128', 'hal-03540209', 'nnt2021coaz0011'],
+  person: ['idref227790677', 'idref257915222'],
 };
